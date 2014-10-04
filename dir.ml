38d19965f590d70d21725414fc8749dcf0413666
vvzakharenko <vvzakharenko@gmail.com>
2014-10-04 23:22:44 +0300

    remove x y of_string from dir

--- dir.ml
+++ dir.ml
@@ -5,11 +5,6 @@ type t = | Up
 	 | Leftdown
 	 | Leftup
 	 
-module Turn = struct
-  let t = | Left
-          | Right 
-end
-
 let left = function
   | Up        -> Leftup
   | Leftup    -> Leftdown
@@ -39,32 +34,3 @@ let mirror =
   | Leftdown  -> Rightup
   | Rightdown -> Leftup
   | Leftup    -> Rightdown
-
-let x dir = (+)
-  (match dir with
-   | Up        -> 0
-   | Down      -> 0
-   | Rightup   -> 1
-   | Rightdown -> 1
-   | Leftdown  -> -1
-   | Leftup    -> -1)
-
-let y dir y =
-  let q = if y % 2 = 0 
-          then 1 
-          else 0 in
-  match dir with
-  | Up        -> y + q 
-  | Rightup   -> y + q 
-  | Leftup    -> y + q
-  | Rightdown -> y + q - 1 
-  | Down      -> y + q - 1
-  | Leftdown  -> y + q - 1
-
-let of_string = function
-  | "Up"        -> Up
-  | "Rightup"   -> Rightup
-  | "Rightdown" -> Rightdown
-  | "Down"      -> Down
-  | "Leftdown"  -> Leftdown
-  | "Leftup"    -> Leftup

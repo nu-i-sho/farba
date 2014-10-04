4514489057a5cfd6277cc1eab7edecfdba978c96
Miray Mann <miray.mann@onnr.co>
2014-10-04 23:22:44 +0300

    remove x y of_string from dir

--- dir.mli
+++ dir.mli
@@ -1,8 +1,7 @@
 type t
 
+val turn   : t -> Turn.t -> t
 val left   : t -> t
 val right  : t -> t
 val mirror : t -> t
-val turn   : t -> Turn.t -> t
-val x      : t -> int -> int
-val y      : t -> int -> int
+

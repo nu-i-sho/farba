8a0fef1e5534dca3818d58953271fde12be5fc1e
vvzakharenko <vvzakharenko@gmail.com>
2014-10-07 02:11:21 +0300

    extract link to LINK.T

--- link.mli
+++ link.mli
@@ -1,8 +1,1 @@
-module Make (Key : ORDERED.T) (Value : EMPTIBLE.T) = sig
-  include EMPTIBLE.T 
-	
-  val make_with : Value.t -> t
-  val value_of  : t -> Value.t
-  val go_from   : t -> ~by:Key.t -> t
-  val link      : t -> ~to':t -> ~by:Key.t -> t
-end
+include LINK.T

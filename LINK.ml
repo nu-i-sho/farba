52798f94f7f696fcca2245e44ade798514839eec
Miray Mann <miray.mann@onnr.co>
2014-10-07 02:11:21 +0300

    extract link to LINK.T

---
+++ LINK.ml
@@ -0,0 +1,11 @@
+module type T = sig
+  module Make (Key : ORDERED.T) (Value : EMPTIBLE.T) = sig
+    
+    include EMPTIBLE.T 
+	
+    val make_with : Value.t -> t
+    val value_of  : t -> Value.t
+    val go_from   : t -> ~by:Key.t -> t
+    val link      : t -> ~to':t -> ~by:Key.t -> t
+  end
+end

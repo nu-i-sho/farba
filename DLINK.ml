9d6f424c06f6f7356ac0f34b7d5b1c70f64840dc
Miray Mann <miray.mann@onnr.co>
2014-10-11 23:50:18 +0300

    reimplement DLINK for 'a t and remove MAKE_T from

--- DLINK.ml
+++ DLINK.ml
@@ -1,8 +1,4 @@
 module type T = sig
   include INTEROPPOSITION_LINK.T with type key_t = Hand.t
-
-  val load_from value_t Array.t
+  val load_from : 'a List.t
 end
-
-module type MAKE_T = functor
-    (Value : EMPTIBLE.T) -> T with type value_t = Value.t

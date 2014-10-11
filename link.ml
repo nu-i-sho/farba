3c85f53c18bff4b81623e3b1cef5fe72b8c5d430
vvzakharenko <vvzakharenko@gmail.com>
2014-10-11 23:13:16 +0300

    reimplement link with 'a t

--- link.ml
+++ link.ml
@@ -1,35 +1,26 @@
 module Make : LINK.MAKE_T = functor 
-  (Key : ORDERED.T) (Value : EMPTIBLE.T) = struct
+  (Key : ORDERED.T) = struct
   
     module Links = Map.Make(Key)
 	
-    type t = { links : t Links.t;
-	       value : value_t
-	     }
+    type 'a t = { links : t Links.t;
+		  value : 'a
+		}
 
-    type key_t   = Key.t
-    type value_t = Value.t
-
-    let empty = 
-      { links = Links.empty;
-	value = Value.empty
-      }
+    type key_t = Key.t
 	
     let make_with value = 
       { links = Links.empty;
 	value
       }
 
-    let value_of link = 
-      link.value
-
-    let go_from link ~by:key =
-      if   link.links |> Links.mem  key 
-      then link.links |> Links.find key 
-      else empty
+    let go_from link ~by:key = 
+      link.links |> LinksMap.find key 
 
     let link linker ~to':linkable ~by:key = 
-      { linker with links = Links.add key linkable linker.links
-      }
-	
+      { linker with links = LinksMap.add key linkable linker.links
+      }	
+
+    let is_impasse link ~by:key =
+      link.links |> LinksMap.mem key	
   end

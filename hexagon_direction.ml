c9d50c2b238682be2602833d04cf658631be6348
Miray Mann <miray.mann@onnr.co>
2014-10-12 00:19:25 +0300

    reimplement Hexagon_direction with Direction.Make using

--- hexagon_direction.ml
+++ hexagon_direction.ml
@@ -1,36 +1,11 @@
-type t = | Up
-	 | Rightup
-	 | Rightdown
-	 | Down
-	 | Leftdown
-	 | Leftup
-	 
-let left = 
+include Direction.Make(struct
+  type t = | Up
+           | Rightup
+	   | Rightdown
+	   | Down
+	   | Leftdown
+	   | Leftup
 
-let right = 
-
-let turn = 
-  let open Turn in 
-  function
-  | Left  -> function
-      | Up        -> Leftup
-      | Leftup    -> Leftdown
-      | Leftdown  -> Down
-      | Down      -> Rightdown
-      | Rightdown -> Rightup
-      | Rightup   -> Up
-  | Right -> function
-      | Up        -> Rightup
-      | Rightup   -> Rightdown
-      | Rightdown -> Down
-      | Down      -> Leftdown
-      | Leftdown  -> Leftup
-      | Leftup    -> Up
-
-let mirror =
-  | Up        -> Down
-  | Down      -> Up
-  | Rightup   -> Leftdown
-  | Leftdown  -> Rightup
-  | Rightdown -> Leftup
-  | Leftup    -> Rightdown
+  let all_ordered_to_right = 
+    [Down; Leftdown; Leftup; Up; Rightup; Rightdown]
+end)

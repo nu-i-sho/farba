109ce89fbca920799a480bf8ca02d0442b4f6b8d
vvzakharenko <vvzakharenko@gmail.com>
2014-10-11 23:40:31 +0300

    add SEED_T and MAKE_T to DIRECTION

--- DIRECTION.ml
+++ DIRECTION.ml
@@ -2,3 +2,12 @@ module type T = sig
   include ORDERABLE_AND_OPPOSABLE.T
   val turn : t -> ~to':Hand.t -> t
 end
+
+module type SEED_T = sig
+  type t
+  val all_ordered_to_right : t List.t
+end
+
+module type MAKE_T = functor
+    (Seed : SEED_T) -> 
+      T with type t = Seed.t

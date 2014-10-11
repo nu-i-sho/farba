b51cf126e56a1d80f44417569c07c10a125592d4
Miray Mann <miray.mann@onnr.co>
2014-10-11 23:29:14 +0300

    reimplement interoposition_link for 'a t

--- interopposition_link.ml
+++ interopposition_link.ml
@@ -1,9 +1,7 @@
-module Make : INTEROPPOSITION_LINK.MAKE.T = functor
-  (Key : ORDERED_AND_OPPOSABLE.T)
-    (Value : EMPTIBLE.T) = 
-  struct
-    include Link.Make (Key) (Value)
-
+module Make : INTEROPPOSITION_LINK.MAKE_T = functor
+  (Key : ORDERABLE_AND_OPPOSABLE.T) = struct
+    include Link.Make (Key)
+	
     let join a ~with':b ~by:key = 
       let rec a' = link a ~to':b' ~by:key
           and b' = link b ~to':a' ~by:(Key.opposite key) in

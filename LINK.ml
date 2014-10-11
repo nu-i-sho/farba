1b077a52e55e27e19f9590068c360dc235fd0562
Miray Mann <miray.mann@onnr.co>
2014-10-11 23:11:42 +0300

    fix some module types using in LINK

--- LINK.ml
+++ LINK.ml
@@ -1,8 +1,8 @@
 module type T = sig
-  include READONLY_LINK.T
+  include MAKEABLE_READONLY_LINK.T
   val link : 'a t -> ~to':('a t) -> ~by:key_t -> 'a t
 end
 
 module type MAKE_T = functor 
-    (Key : ORDERED.T) -> 
+    (Key : ORDERABLE.T) -> 
       T with type key_t = Key.t

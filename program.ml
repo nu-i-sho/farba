a7aa0f0d01325c7b6c89e1e938a9bc554c7984d6
Miray Mann <miray.mann@onnr.co>
2014-10-11 21:23:04 +0300

    save program craft with 'a t using in history

--- program.ml
+++ program.ml
@@ -1,81 +1,62 @@
-module type T = sig
-  type t
-end
-
-module type EMPTIBLE = sig
-  include T
-  val empty : t
-end
-
 module type READONLY_LINK = sig
-  include EMPTIBLE
-
+  type 'a t
   type key_t
-  type value_t
-  
-  val value_of      : t -> value_t
-  val go_from       : t -> by:key_t -> t
-  val is_impasse_by : key_t -> bool 
+
+  val value_of   : 'a t -> 'a
+  val go_from    : 'a t -> by:key_t -> 'a t
+  val is_impasse : 'a t -> by:key_t -> bool 
 end
 
 module type MAKEABLE_READONLY_LINK = sig
   include READONLY_LINK
-
-  val make_with : value_t -> t    
+  
+  val make_with : 'a -> 'a t    
 end
 
 module type LINK = sig
   include MAKEABLE_READONLY_LINK
 
-  val link : t -> to':t -> by:key_t -> t
+  val link : 'a t -> to':('a t) -> by:key_t -> 'a t
 end
 
 module type ORDERED = sig
-  include T
-  val compare : t -> t -> int
+  type 'a t
+  val compare : 'a t -> 'a t -> int
 end
 
-module type MAKE_LINK = functor 
-    (Key : ORDERED) 
-      (Value : EMPTIBLE) -> 
-	LINK with type key_t = Key.t and value_t = Value.t
+module type MAKE_LINK = 
+    functor (Key : ORDERED) -> 
+      LINK with type key_t = Key.t
 
-module MakeLink : MAKE_LINK = functor 
-  
-  (Key : ORDERED) 
-    (Value : EMPTIBLE) = struct
+module MakeLink : MAKE_LINK = 
+  functor (Key : ORDERED) = struct
       
-      module LinksMap = Map.Make(Key)
+    module LinksMap = Map.Make(Key)
 	
-      type t = { links : t LinksMap.t;
-		 value : value_t
-	       }
+    type 'a t = { links : 'a t LinksMap.t;
+		  value : 'a
+		}
 
-      type key_t   = Key.t
-      type value_t = Value.t
-
-      let empty = 
-	{ links = LinksMap.empty;
-	  value = Value.empty
-	}
+    type key_t = Key.t
 	
-      let make_with value = 
-	{ links = LinksMap.empty;
-	  value
-	}
+    let make_with value = 
+      { links = LinksMap.empty;
+	value
+      }
 
-      let value_of link = 
-	link.value
+    let value_of link = 
+      link.value
 
-      let go_from link by:key =
-	if   link.links |> LinksMap.mem  key 
-	then link.links |> LinksMap.find key 
-	else empty
+    let go_from link ~by:key = 
+      link.links |> LinksMap.find key 
 
-      let link linker to':linkable by:key = 
-	{ linker with links = LinksMap.add key linkable linker.links
-	}	
-    end
+    let link linker ~to':linkable ~by:key = 
+      { linker with links = LinksMap.add key linkable linker.links
+      }	
+
+    let is_impasse link ~by:key =
+      link.links |> LinksMap.mem key
+  end
 
 module type INTEROPPOSITION_LINK = sig
   include MAKEABLE_READONLY_LINK
@@ -84,7 +65,7 @@ module type INTEROPPOSITION_LINK = sig
 end
 
 module type OPPOSABLE = sig
-  include T
+  type t
   val opposite : t -> t
 end
 
@@ -93,28 +74,19 @@ module type ORDERED_AND_OPPOSABLE = sig
   include OPPOSABLE
 end
 
-module type MAKE_INTEROPPOSITION_LINK = functor 
-    (Key : ORDERED_AND_OPPOSABLE)
-      (Value : EMPTIBLE) -> 
-	INTEROPPOSITION_LINK with type key_t = Key.t and value_t = Value.t
+module type MAKE_INTEROPPOSITION_LINK = 
+    functor (Key : ORDERED_AND_OPPOSABLE) -> 
+      INTEROPPOSITION_LINK with type key_t = Key.t
 
-module MakeInterOppositionLink : MAKE_INTEROPPOSITION_LINK = functor
-  
-  (Key : ORDERED_AND_OPPOSABLE)
-    (Value : EMPTIBLE) = struct
-      
-      include Link.Make (Key) (Value)
-
-      let join a ~with':b ~by:key = 
-	let rec a' = link a ~to':b' ~by:key
-            and b' = link b ~to':a' ~by:(Key.opposite key) in
-	a'
-    end  
-
-module Hand = struct
-  type t = | Left
-           | Right
-end
+module MakeInterOppositionLink : MAKE_INTEROPPOSITION_LINK = 
+  functor (Key : ORDERED_AND_OPPOSABLE) = struct
+    include MakeLink (Key)
+	
+    let join a ~with':b ~by:key = 
+      let rec a' = link a ~to':b' ~by:key
+          and b' = link b ~to':a' ~by:(Key.opposite key) in
+      a'
+  end  
 
 module type DIRECTION = sig
   include ORDERED_AND_OPPOSABLE
@@ -124,114 +96,134 @@ end
 
 module type DIRECTION_SEED = sig
   type t
-  val link_to_start_of_directions_roundelay
-      : RoundelayLink with type value_t = t
+  val all_ordered_to_right : t List.t
 end
 
 module type MAKE_DIRECTION = functor
     (Seed : DIRECTION_SEED) -> 
       DIRECTION with type t = Seed.t
 
-module MakeDirection : MAKE_DIRECTION = functor
-  (Seed : DIRECTION_SEED) = struct
-    open Hand
-
-    module Link = MakeRoundelayLink (Seed)
-
-    type t = Seed.t
-
-    let start = 
-      Seed.link_to_start_of_directions_roundelay
-
-    let count = 
-      let rec count' current acc =
-        if current = (Link.value_of start) && acc <> 0
-	then acc
-	else count' 
-	    (Link.go_from current ~by:Righ) 
-	    (acc + 1) 
-      in
-      count' start 0
-
-    let opposite direction =
-      let index_of_opposite_direction = count / 2 in
-      let rec opposite' current i  =
-	if i = index_of_opposite_direction
-	then Link.value_of current
-	else opposite' 
-	    (Link.go_from current ~by:Right)
-	    (i + 1)	    
-      in
-      opposite' start 0 
-
-    let compare x y =
-      let index_of direction =
-	let rec index_of' current acc =
-	  if direction = Link.value_of current
-	  then acc 
-	  else index_of'
-	      (Link.go_from current ~by:Right)
-	      (acc + 1) 
-	in 
-	index_of' start 0 in
-      Pervasives.compare
-	(index_of x)
-	(index_of y)
-	  
-    let turn direction ~to':hand = 
-      let rec find ~current =
-	if direction = Link.value_of current
-	then current
-	else find (Link.go_from current ~by:Right)
-      in
-      Link.go_from
-	(find ~current:start)
-	~by:hand
-  end
-
 module type DLINK = sig
   include INTEROPPOSITION_LINK with type key_t = Hand.t
 
-  val load_from : value_t List.t
+  val load_from : 'a List.t
 end
 
-module type MAKE_DLINK = functor
-    (Value : EMPTIBLE) -> 
-      DLINK with type key_t = Hand.t and value_t = Value.t
+module Dlink : DLINK = struct
+  include MakeInterOppositionLink (Hand)
+  open Hand
 
-module MakeDlink : MAKE_DLINK = functor
-  (Value : EMPTIBLE) = struct
-    include MakeInterOppositionLink (Hand) (Value)
-    open Hand
-
-    let load_from source = 
-      let links = List.map make_with source in
-      List.fold_left 
-	(fun a b -> join a ~with':b ~by:Right)
-	List.hd links
-	List.tl links	
+  let load_from source = 
+    let links = List.map make_with source in
+    List.fold_left 
+      (fun a b -> join a ~with':b ~by:Right)
+      List.hd links
+      List.tl links
 end
 
 module type ROUNDELAY_LINK = sig
   include READONLY_LINK with type key_t = Hand.t
 
-  val close : value_t 
+  val close : 'a Dlink.t -> 'a t 
 end
 
-module type MAKE_ROUNDELAY_LINK = functor
-    (Direction : DIRECTION) -> sig
-      include READONLY_LINK with type key_t = Hand.t
+module HandSeed : DIRECTION_SEED = struct
+  type t = | Left
+           | Right
 
-      val close : MakeDlink (Direction).t -> t
-    end 
+  let all_ordered_to_right = 
+    [Left; Right]
+end
 
-module type MakeRoundDelayLink : MAKE_ROUNDELAY_LINK = functor
-    (Direction : DIRECTION) = struct
-      include MakeDlink (Direction)
+module SquereDirectionSeed : DIRECTION_SEED = struct 
+  type t = | Up
+           | Down 
+           | Left
+           | Right
 
-      let close dlink =
-	f
+  let all_ordered_to_right = 
+    [Down; Left; Up; Right]
+end
+
+module HexagonDirectionSeed : DIRECTION_SEED = struct
+  type t = | Up
+	   | Rightup
+	   | Rightdown
+	   | Down
+	   | Leftdown
+	   | Leftup
+
+  let all_ordered_to_right = 
+    [Down; Leftdown; Leftup; Up; Rightup; Rightdown]
+end
+
+module Hand : DIRECTION = MakeDirection (HandSeed)
+   and MakeDirection : MAKE_DIRECTION = functor
+    (Seed : DIRECTION_SEED) = struct
+      open Hand
+
+      type t = Seed.t
+
+      let start =
+	RoundelayLink.close 
+	  (Dlink.load_from 
+	     Seed.all_ordered_to_right) 
+
+      let opposite_index = 
+	let start_value = Dlink.value_of start in
+	let rec count current acc =
+          if current = start_value && acc <> 0
+	  then acc
+	  else count 
+	      (Dlink.go_from current ~by:Righ) 
+	      (acc + 1) 
+	in
+	(count start 0) / 2
+
+      let opposite direction =
+	let rec opposite' current i  =
+	  if i = opposite_index
+	  then Dlink.value_of current
+	  else opposite' 
+	      (Dlink.go_from current ~by:Right)
+	      (i + 1)	    
+	in
+	opposite' start 0 
+
+      let compare x y =
+	let index_of direction =
+	  let rec index_of' current acc =
+	    if direction = Dlink.value_of current
+	    then acc 
+	    else index_of'
+		(Dlink.go_from current ~by:Right)
+		(acc + 1) 
+	  in 
+	  index_of' start 0 in
+	Pervasives.compare
+	  (index_of x)
+	  (index_of y)
+	  
+      let turn direction ~to':hand = 
+	let rec find ~current =
+	  if direction = Link.value_of current
+	  then current
+	  else find (Link.go_from current ~by:Right)
+	in
+	Link.go_from
+	  (find ~current:start)
+	  ~by:hand
     end
+
+module SquereDirection : DIRECTION =
+  MakeDirection (SquereDirectionSeed)
+
+module HexagonDirection : DIRECTION = 
+  MakeDirection (HexagonDirectionSeed)
+
+
+
+
 
 
 

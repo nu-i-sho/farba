6983e0e27df9a478d9966d330b2f60a7a6109402
vvzakharenko <vvzakharenko@gmail.com>
2014-10-12 22:43:42 +0300

    fix farba signature

--- farba.mli
+++ farba.mli
@@ -1,8 +1,1 @@
-module Make (Direction : DIRECTION.T) = sig
-  type t
-
-  val place_of     : t -> Place.t
-  val direction_of : t -> Direction.t
-  val fill_of      : t -> Fill.t 
-  val do_with      : t -> command:Command.t -> t
-end
+module Make : FARBA.MAKE_T

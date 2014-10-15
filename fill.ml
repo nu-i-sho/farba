243ec16517d4488be0d8d0c88f470b5164ec4888
Miray Mann <miray.mann@onnr.co>
2014-10-16 01:02:07 +0300

    introduce fill as EMPTIBLE_AND_CHAR_PARSABLE

--- fill.ml
+++ fill.ml
@@ -1,2 +1,8 @@
-type t = | Color of Color.t
-         | Transparent
+type t =  | Color of Color.t
+          | Transparent  
+let empty = Transparent
+
+type source_t  = char
+let parse_from = function 
+  | '-' -> Transparent
+  | oth -> Color (Color.parse_from oth)

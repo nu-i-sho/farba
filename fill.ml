bd606c86e2df85702b7059356dd98161c75af6ba
Miray Mann <miray.mann@onnr.co>
2014-10-17 00:39:10 +0300

    reimplement Fill as EMPTIBLE.T only

--- fill.ml
+++ fill.ml
@@ -1,8 +1,4 @@
-type t =  | Color of Color.t
-          | Transparent  
-let empty = Transparent
+type t = | Color of Color.t
+         | Transparent  
 
-type source_t  = char
-let parse_from = function 
-  | '-' -> Transparent
-  | oth -> Color (Color.parse_from oth)
+let empty = Transparent

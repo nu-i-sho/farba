76348c5de7c5db4898c6b0213f939151c80c8c16
Miray Mann <miray.mann@onnr.co>
2014-10-17 00:06:12 +0300

    add default to DIRECTION.T

--- DIRECTION.ml
+++ DIRECTION.ml
@@ -1,11 +1,12 @@
 module type T = sig
   include ORDERABLE_AND_OPPOSABLE.T
-  val turn : t -> ~to':Hand.t -> t
+  val turn    : t -> ~to':Hand.t -> t
+  val default : t
 end
 
 module type SEED_T = sig
   type t
-  val all_ordered_to_right : t List.t
+  val all_from_default_ordered_to_right : t List.t
 end
 
 module type MAKE_T = functor

59a148212b3c5fff812c17b358dd1c43162abb3b
vvzakharenko <vvzakharenko@gmail.com>
2014-10-20 00:11:16 +0300

    remove EXTENDET_LINK

--- EXTENDET_LINK.ml
+++
@@ -1,8 +0,0 @@
-module type MAKE_T = functor 
-    (Base : LINK.T) = functor 
-    (Extention : LINK_PLUS.T) = struct
-      type 'a t
-      type key_t
-      include Base      with type 'a t := 'a t and key_t = key_t
-      include Extention with type 'a t := 'a t and key_t = key_t
-    end

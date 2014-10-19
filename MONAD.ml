380ffbdbd49758f8ab7f118eca1b876b0ddcbc7b
vvzakharenko <vvzakharenko@gmail.com>
2014-10-19 20:42:05 +0300

    replace '

--- MONAD.ml
+++ MONAD.ml
@@ -1,5 +1,5 @@
 module type T = sig
-  type ‘a t
-  val bind : ‘a t -> (‘a -> ‘b t) -> ‘b t
-  val return : ‘a -> ‘a t
+  type 'a t
+  val bind : 'a t -> ('a -> 'b t) -> 'b t
+  val return : 'a -> 'a t
 end

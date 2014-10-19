d89a3982e97d417572d6a2f3db471e51f7c9ff33
vvzakharenko <vvzakharenko@gmail.com>
2014-10-20 00:04:29 +0300

    introduce LINK_PLUS.ml

--- LINK_PLUS.ml
+++ LINK_PLUS.ml
@@ -3,5 +3,5 @@ module type T = sig
   type key_t
 
   val go_to_end_from : 'a t -> by:key_t
-  val go_from        : 'a t -> by:key_t -> ~steps_count:int
+  val go_from        : 'a t -> by:key_t -> steps_count:int
 end

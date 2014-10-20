f8bbb45c977cf57c05540c80b5e500cc42a14489
vvzakharenko <vvzakharenko@gmail.com>
2014-10-20 23:59:55 +0300

    introduce new signature of READONLY_LINK.T

--- READONLY_LINK.ml
+++ READONLY_LINK.ml
@@ -2,7 +2,9 @@ module type T = sig
   type 'a t
   type key_t
 
-  val value_of  : t -> 'a
-  val go_from   : t -> ~by:key_t -> t
-  val is_impasse : 'a t -> by:key_t -> bool
+  val value_of       : 'a t -> 'a
+  val get_from       : 'a t -> by:key_t -> 'a t
+  val is_impasse     : 'a t -> by:key_t -> bool
+  val go_from        : 'a t -> by:key_t -> steps_count:int -> 'a t
+  val go_to_end_from : 'a t -> by:key_t -> 'a t
 end

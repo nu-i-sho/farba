f157521a1c160de90f3d9b5fb0c548b12147ed1b
vvzakharenko <vvzakharenko@gmail.com>
2014-10-24 01:26:03 +0300

    fix functor syntax (replace = with ->) in InteroppositionLink

--- interoppositionLink.ml
+++ interoppositionLink.ml
@@ -1,9 +1,9 @@
 module Make : INTEROPPOSITION_LINK.MAKE_T = functor
-  (Key : ORDERABLE_AND_OPPOSABLE.T) = struct
+  (Key : ORDERABLE_AND_OPPOSABLE.T) -> struct
     include Link.Make (Key)
 	
     let join a ~with':b ~by:key = 
       let rec a' = link a ~to':b' ~by:key
           and b' = link b ~to':a' ~by:(Key.opposite key) in
       a'
-  end   
+  end

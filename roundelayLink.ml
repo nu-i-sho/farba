ee07182b436abffda5c857d088a82101db968e6f
Miray Mann <miray.mann@onnr.co>
2014-10-24 22:27:21 +0300

    do RoundelayLink compilable

--- roundelayLink.ml
+++ roundelayLink.ml
@@ -2,11 +2,10 @@ include Dlink
 
 let close link =
   let rec go_from' current ~to_limit_of:direction =
-    if is_impasse current ~by:direction
-    then current
-    else go_from'
-	(go_from current)
-	~to_limit_of:direction
+    if is_impasse current ~by:direction then current else 
+    go_from'
+      (get_from current ~by:direction)
+      ~to_limit_of:direction
   in
   let open Hand in
   let first = go_from' link ~to_limit_of:Left  in

79c6f3ce66685533cacfaabadb5c40fc5c539a75
vvzakharenko <vvzakharenko@gmail.com>
2014-10-25 18:45:57 +0300

    introduce LINK.MAKE_EXT_T (Key : ORDERABLE.T) (Links : Map.S with type key = Key.t)

--- LINK.ml
+++ LINK.ml
@@ -3,6 +3,16 @@ module type T = sig
   val link : 'a t -> to':('a t) -> by:key_t -> 'a t
 end
 
+module type MAKE_EXT_T = functor
+    (Key : ORDERABLE.T) -> functor
+      (Links : Map.S with type key = Key.t) -> sig
+	type 'a t = { links : 'a t Links.t;
+		      value : 'a
+		    }
+
+	include T with type 'a t := 'a t and type key_t = Key.t
+      end
+
 module type MAKE_T = functor 
     (Key : ORDERABLE.T) -> 
       T with type key_t = Key.t

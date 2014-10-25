c326b76292c3bdd95a3a84a6f78804f308181fb3
Miray Mann <miray.mann@onnr.co>
2014-10-25 20:25:40 +0300

    do Direction compilable

--- direction.ml
+++ direction.ml
@@ -1,54 +1,52 @@
-module Make (Seed : DIRECTION_SEED) = struct
-  open Hand
+module Make : DIRECTION.MAKE_T = functor 
+  (Seed : DIRECTION.SEED_T) -> struct
 
-  type t = Seed.t
+    open RoundelayLink
+    open Hand
 
-  let default =
-    RoundelayLink.close 
-      (Dlink.load_from 
-	 Seed.all_from_default_ordered_to_right) 
+    type t = Seed.t
 
-  let opposite_index = 
-    let rec count current acc =
-      if current = Dlink.value_of default && acc <> 0
-      then acc
-      else count 
-	  (Dlink.go_from current ~by:Righ) 
-	  (acc + 1) 
-    in
-    (count default 0) / 2
+    let default =
+      Seed.all_from_default_ordered_to_right
+        |> Dlink.load
+        |> RoundelayLink.close 
 
-  let opposite direction =
-    let rec opposite' current i  =
-      if i = opposite_index
-      then Dlink.value_of current
-      else opposite' 
-	  (Dlink.go_from current ~by:Right)
-	  (i + 1)	    
-    in
-    opposite' default 0 
+    let opposite_index = 
+      let rec count current acc =
+	if value_of current = 
+	   value_of default && acc <> 0 then acc 
+	else 
+	   count (get_from current ~by:Right) 
+	         (acc + 1) 
+      in
+      (count default 0) / 2
 
-  let compare x y =
-    let index_of direction =
-      let rec index_of' current acc =
-	if direction = Dlink.value_of current
-	then acc 
-	else index_of'
-	    (Dlink.go_from current ~by:Right)
-	    (acc + 1) 
-      in 
-      index_of' default 0 in
-    Pervasives.compare
-      (index_of x)
-      (index_of y)
+    let opposite direction =
+      let rec opposite' current i  =
+	if i = opposite_index then 
+	  value_of current 
+	else opposite' (go_from current ~by:Right)
+	               (i + 1) 
+      in
+      opposite' default 0 
+
+    let compare x y =
+      let index_of direction =
+	let rec index_of' current acc =
+	  if direction = value_of current then acc else
+	     index_of'(go_from current ~by:Right)
+	              (acc + 1) 
+	in 
+	index_of' default 0 in
+      Pervasives.compare (index_of x)
+	                 (index_of y)
 	  
-  let turn direction ~to':hand = 
-    let rec find ~current =
-      if direction = Dlink.value_of current
-      then current
-      else find (Dlink.go_from current ~by:Right)
-    in
-    Dlink.go_from
-      (find ~current:default)
-      ~by:hand
-end
+    let turn direction ~to':hand = 
+      let rec find ~current =
+	if direction = value_of current then current else
+	   find (go_from current ~by:Right) in
+      go_from (find ~current:default)
+              ~by:hand
+  end
+
+

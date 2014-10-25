09d848368f3c6027245112a50768e9e4de2c579e
vvzakharenko <vvzakharenko@gmail.com>
2014-10-25 20:27:31 +0300

    save InteroppositionLink implemented with Link.MakeForExtend in history

--- interoppositionLink.ml
+++ interoppositionLink.ml
@@ -1,9 +1,10 @@
 module Make : INTEROPPOSITION_LINK.MAKE_T = functor
   (Key : ORDERABLE_AND_OPPOSABLE.T) -> struct
-    include Link.Make (Key)
-	
+    module Links = Map.Make (Key)
+    include Link.MakeForExtend (Key) (Links)
+
     let join a ~with':b ~by:key = 
-      let rec a'() = link a ~to':(b'()) ~by:key
-          and b'() = link b ~to':(a'()) ~by:(Key.opposite key) in
-      a'()
+      let rec a' = { a with links = a.links |> Links.add key b' }
+          and b' = { b with links = b.links |> Links.add (Key.opposite key) a' }
+      in a'
   end

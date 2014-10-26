aff29d9252eedbf6a7d6b7a3c709010fbea55dfd
vvzakharenko <vvzakharenko@gmail.com>
2014-10-26 13:32:51 +0200

    fix synax error in PARSABLE-FARBA.T

--- PARSABLE_FARBA.ml
+++ PARSABLE_FARBA.ml
@@ -1,6 +1,6 @@
 module type T = sig
   type t
   include FARBA.T    with type t := t
-  include PARSABLE.T with type t := t and source_t = string
+  include PARSABLE.T with type t := t and type source_t = string
 end
 

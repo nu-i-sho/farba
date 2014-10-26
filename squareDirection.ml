87a85ee42dfc1e9d07a901555a97d2a668150bb1
vvzakharenko <vvzakharenko@gmail.com>
2014-10-26 16:42:44 +0200

    reimplement SquareDirection with SquareDirectionSeed using

--- squareDirection.ml
+++ squareDirection.ml
@@ -1,9 +1,1 @@
-include Direction.Make (struct 
-  type t = | Up
-           | Down 
-           | Left
-           | Right
-
-  let all_from_default_ordered_to_right = 
-    [Up; Right; Down; Left]
-end)
+include Direction.Make (SquareDirectionSeed)

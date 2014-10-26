4751a59de5bcf588f762fdbc1c5276482da48530
vvzakharenko <vvzakharenko@gmail.com>
2014-10-26 13:36:20 +0200

    do Farba compilable

--- farba.ml
+++ farba.ml
@@ -1,15 +1,18 @@
-module Make = functor 
-  (Direction : DIRECTION.T) = functor 
-  (BoardLink : READONLY_LINK.T with type key_t = Direction.t) = struct
+module Make  : FARBA.MAKE_T = functor 
+  (Direction : DIRECTION.T) -> functor 
+  (BoardLink : READONLY_LINK.T with type key_t = Direction.t) -> struct
    
-    type t = { place_link : Place.t BoardLink.t;
-               diretion   : Direction.t;
+    type 'a board_link_t = 'a BoardLink.t
+    type direction_t = Direction.t
+
+    type t = { place_link : Place.t board_link_t;
+               direction  : direction_t;
                fill       : Fill.t
              }
-
+	  
     let make board_link  = { place_link = board_link;
 			     direction  = Direction.default;
-			     fill       = Fill.Transparent
+			     fill       = Fill.empty
 			   }
 
     let place_of farba         = BoardLink.value_of farba.place_link
@@ -27,7 +30,7 @@ module Make = functor
       match command with
       | Fill fill -> { farba with fill }
       | Turn hand -> { farba with direction = turn direction ~to':hand }
-      | Replicate -> { farba with place = value_of (go_from place ~by:direction);
+      | Replicate -> { farba with place_link = get_from place_link ~by:direction;
 		                  direction = opposite direction
                      }
 end

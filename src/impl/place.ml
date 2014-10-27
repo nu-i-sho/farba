463d12fad4164b8b36df7aff018a891e6f4b63bb
Miray Mann <miray.mann@onnr.co>
2014-10-27 07:09:26 +0200

    reimplement Place for EMPTIBLE_AND_FROM_CHAR_MAKEABLE.MAKE_T

--- src/impl/place.ml
+++ src/impl/place.ml
@@ -1,9 +1,13 @@
-type t = | Cell of Cell.t
-         | Empty
+module Make : EMPTIBLE_AND_CHAR_PARSABLE.MAKE_T = functor
+  (Cell : FROM_CHAR_MAKEABLE.T) -> struct
+    
+    type t = | Cell of Cell.t
+             | Empty
 
-let empty = Empty
+    let empty = Empty
 
-type source_t = char
-let parse = function 
-  | '.' -> empty
-  | oth -> Cell (Cell.parse oth)
+    type source_t = char
+    let make = function 
+      | '.' -> empty
+      | oth -> Cell (Cell.make oth)
+  end

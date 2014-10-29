dc3bb04d8006173ac2370527335a8f7d548e53db
vvzakharenko <vvzakharenko@gmail.com>
2014-10-29 23:30:20 +0200

    introduce TESTS_RUNNER

---
+++ src/tests/TESTS_RUNNER.ml
@@ -0,0 +1,14 @@
+module type T = sig
+  type output_t
+  type message_t
+  type testSession_t
+
+  val run  : testSession_t list -> output:output_t -> ()
+  val exec : testSession_t list -> message_t list
+end
+
+module type MAKE_T = functor
+    (Output : OBSERVER.T with type message_t = TestMessage.t) ->
+      T with type output_t = Output.t 
+         and type message_t = TestMessage.t
+	 and type testSession_t = (module TESTS_SESSION.T)

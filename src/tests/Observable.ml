e1d3f6669b31b8616a4b70d3c86e06930b58830d
vvzakharenko <vvzakharenko@gmail.com>
2014-10-31 02:59:10 +0200

    rename Map to M in Observable

--- src/tests/Observable.ml
+++ src/tests/Observable.ml
@@ -9,8 +9,8 @@ type 'a t = { map : 'a M.t;
 
 let subscribe observer o = 
   let max = o.max + 1 in
-  let o = { o with map = o.map |> Map.add max observer; max } in
+  let o = { o with map = o.map |> M.add max observer; max } in
   let unsibscribe () =
-    let o = { o with map = o.map |> Map.remove max observer } in
+    let o = { o with map = o.map |> M.remove max observer } in
     (observer, o) in
   unsubscribe

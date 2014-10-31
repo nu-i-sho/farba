825e5417713df43a867e8f663f59af36109e2d8d
Miray Mann <miray.mann@onnr.co>
2014-10-31 02:51:13 +0200

    remove Make from Observer

--- src/tests/Observable.ml
+++ src/tests/Observable.ml
@@ -1,19 +1,16 @@
-module Make : OBSERVABLE.T = struct 
-  
-  module M = Map.Make (struct
-    type t = int
-    let compare = Pervasives.compare
-  end)
+module M = Map.Make (struct
+  type t = int
+  let compare = Pervasives.compare
+end)
 
-  type 'a t = { map : 'a M.t;
-		max : int
-	      }
+type 'a t = { map : 'a M.t;
+	      max : int
+	    }
 
-  let subscribe observer o = 
-    let max = o.max + 1 in
-    let o = { o with map = o.map |> Map.add max observer; max } in
-    let unsibscribe () =
-      let o = { o with map = o.map |> Map.remove max observer } in
-      (observer, o) in
-    unsubscribe
-end
+let subscribe observer o = 
+  let max = o.max + 1 in
+  let o = { o with map = o.map |> Map.add max observer; max } in
+  let unsibscribe () =
+    let o = { o with map = o.map |> Map.remove max observer } in
+    (observer, o) in
+  unsubscribe

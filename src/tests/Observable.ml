5c90ac139061206360e8e0dbb07fb86cbe153af7
vvzakharenko <vvzakharenko@gmail.com>
2014-10-31 02:44:45 +0200

    fix some mistakes in Observable

--- src/tests/Observable.ml
+++ src/tests/Observable.ml
@@ -1,21 +1,21 @@
-module Make : ONSERVABLE.MAKE_T = struct 
-      type subscribtion_t = int
-      module M = Map.Make (struct
-	type t =  subscribtion_t
-	let compare = Pervasives.compare
-      end)
+module Make : ONSERVABLE.T = struct 
+  
+  module M = Map.Make (struct
+    type t = int
+    let compare = Pervasives.compare
+  end)
 
-      type 'a t = { map : 'a M.t;
-		    max : subscribtion_t
-		  }
+  type 'a t = { map : 'a M.t;
+		max : int
+	      }
 
-      type subscribtion_t = int
+  type subscribtion_t = int
 
-      val subscribe observer o = 
-	let max = o.max + 1 in
-	let o = { o with map = o.map |> Map.add max observer; max } in
-	let unsibscribe () =
-	  let o = { o with map = o.map |> Map.remove max observer } in
-	  (observer, o) in
-	unsubscribe
-    end
+  val subscribe observer o = 
+    let max = o.max + 1 in
+    let o = { o with map = o.map |> Map.add max observer; max } in
+    let unsibscribe () =
+      let o = { o with map = o.map |> Map.remove max observer } in
+      (observer, o) in
+    unsubscribe
+end

7ac82eaead3c32dd94e6bdbb22254b9e72ced760
Miray Mann <miray.mann@onnr.co>
2014-11-07 04:58:59 +0200

    introduce new simple TESTS_RUNNER.T

--- src/tests/TESTS_RUNNER.ml
+++ src/tests/TESTS_RUNNER.ml
@@ -1,22 +1,6 @@
 module type T = sig
   type t
-  type source_t
-
-  val make : (TestMessage.t -> unit) -> t
-  val run  : source_t -> t -> t
-end
-
-module FOR_TEST = struct
-  module type T = T with type 
-	source_t = (module TEST.T)
-end
-
-module FOR_TESTS_SET = struct
-  module type T = T with type 
-	source_t = (module TESTS_SET.T)
-end
-
-module FOR_TESTS_SESSION = struct
-  module type T = T with type 
-	source_t = (module TESTS_SESSION.T)
+  val make  : output:(string -> unit) -> t
+  val run   : (module TEST_FIXTURE.T) -> t -> unit
+  val run_s : (module TEST_FIXTURE.T) list -> t -> unit
 end

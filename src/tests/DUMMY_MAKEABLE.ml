88e024c24d30363a4a69fa623d031ea522182ccd
Miray Mann <miray.mann@onnr.co>
2014-11-26 21:18:29 +0200

    introduce DUMMY_MAKEABLE

---
+++ src/tests/DUMMY_MAKEABLE.ml
@@ -0,0 +1,9 @@
+module type T = sig
+  include MAKEABLE.T
+  val set_make : (source_t, t) -> ()
+end
+
+module type MAKE_T = functor
+    (Product : T.T) -> functor
+      (Source : T.T) -> 
+	T with type t = Product.t and type source_t = Source.t

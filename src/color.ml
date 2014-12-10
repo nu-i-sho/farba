79d0deb80c8377dc53aba07c2a865b917163bdb0
vvzakharenko <vvzakharenko@gmail.com>
2014-12-11 01:52:54 +0200

    simplify Color

--- src/color.ml
+++ src/color.ml
@@ -4,12 +4,3 @@ type t = | Red
 	 | Green
 	 | Blue
 	 | Violet
-
-type source_t = char
-let parse = function 
-  | '0' -> Red
-  | '1' -> Orange
-  | '2' -> Yellow
-  | '3' -> Green
-  | '4' -> Blue
-  | '5' -> Violet

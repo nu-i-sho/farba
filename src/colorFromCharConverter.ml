98458393ebc8d222008c327c9733e815edee1cf3
Miray Mann <miray.mann@onnr.co>
2014-12-11 02:37:23 +0200

    add using Color module to ColorFromCharConverter.convert

--- src/colorFromCharConverter.ml
+++ src/colorFromCharConverter.ml
@@ -4,10 +4,11 @@ type source_t = char
 type product_t = Color.t
 
 let convert chr () =
-  match chr with
-  | '1' -> Red
-  | '2' -> Orange
-  | '3' -> Yellow
-  | '4' -> Green
-  | '5' -> Blue
-  | '6' -> Violet
+  Color.( match chr with
+          | '1' -> Red
+          | '2' -> Orange
+          | '3' -> Yellow
+          | '4' -> Green
+          | '5' -> Blue
+          | '6' -> Violet
+	)

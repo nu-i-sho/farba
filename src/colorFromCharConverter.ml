504dc958990c4c58cb4535e2aa5cb3cdf6702293
vvzakharenko <vvzakharenko@gmail.com>
2014-12-12 00:24:30 +0200

    reimplement ColorFromCharConverter for new CONVERTER.T

--- src/colorFromCharConverter.ml
+++ src/colorFromCharConverter.ml
@@ -1,14 +1,12 @@
-include UnitMakeable
-
-type source_t = char
+type source_t  = char
 type product_t = Color.t
 
-let convert chr () =
-  Color.( match chr with
-          | '1' -> Red
-          | '2' -> Orange
-          | '3' -> Yellow
-          | '4' -> Green
-          | '5' -> Blue
-          | '6' -> Violet
-	)
+let convert = 
+  let open Color in 
+  function | '1' -> Red
+           | '2' -> Orange
+           | '3' -> Yellow
+           | '4' -> Green
+           | '5' -> Blue
+           | '6' -> Violet
+	

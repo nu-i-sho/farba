6b6a0ca7783e812f9271b7d575e9c30349d75df4
Miray Mann <miray.mann@onnr.co>
2014-12-11 02:38:24 +0200

    simplify Cell

--- src/cell.ml
+++ src/cell.ml
@@ -1,14 +1,6 @@
-module Make : EMPTIBLE_AND_FROM_CHAR_MAKEABLE.MAKE_T = functor
-  (Color : FROM_CHAR_MAKEABLE.T) -> struct
+module Make : CELL.MAKE_T = functor (Color : T.T) -> struct
+  include CellState with type color_t = Color.t
+  let empty = Empty
+end
 
-    type t = | Filled of Color.t
-             | Expect of Color.t
-	     | Empty
-
-    let empty = Empty
-
-    type source_t = char
-    let make = function 
-      | '-' -> empty
-      | oth -> Expect (Color.make oth)
-  end
+include Make (Color)

367c6c85178083f0d729051c0a497169c1520132
vvzakharenko <vvzakharenko@gmail.com>
2014-12-11 23:33:34 +0200

    include UnitMakeable to CellFromCharConvertable

--- src/cellFromCharConverter.ml
+++ src/cellFromCharConverter.ml
@@ -3,13 +3,13 @@ module Make : CELL_FROM_CHAR_CONVERTER.MAKE_T =
     functor (Cell : CELL.T with type color_t = Color.t) ->
       functor (CConv : FROM_CHAR_CONVERTER.T  
 	      with type product_t = Color.t) -> 
-  struct
-    type t = unit
+  struct 
+    include UnitMakeable
+
     type source_t = Color.t
     type product_t = Cell.t
 
     let cconv = CConv.make ()
-    let make () = ()
     let convert = function
       | '-' -> fun () -> Cell.empty
       | oth -> fun () -> Cell.Expect (cconv |> CConv.convert oth)

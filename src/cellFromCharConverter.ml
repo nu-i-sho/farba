988bb61557299ba95a0c3dbae59ceda863f4bda2
vvzakharenko <vvzakharenko@gmail.com>
2014-12-11 13:39:26 +0000

    change Make declaration style in СellFromCharConverter

--- src/cellFromCharConverter.ml
+++ src/cellFromCharConverter.ml
@@ -1,8 +1,8 @@
-module Make : CELL_FROM_CHAR_CONVERTER.MAKE_T =
-  functor (Color : T.T) -> 
-    functor (Cell : CELL.T with type color_t = Color.t) ->
-      functor (CConv : FROM_CHAR_CONVERTER.T  
-	      with type product_t = Color.t) -> 
+module Make 
+  (Color : T.T) 
+    (Cell : CELL.T with type color_t = Color.t) 
+      (CConv : FROM_CHAR_CONVERTER.T with type product_t = Color.t) =
+  
   struct
     type t = unit
     type source_t = Color.t

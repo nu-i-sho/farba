b0ff2a9227c2a527a3d4a9d4570ceb8acc52346e
Miray Mann <miraymann@gmail.com>
2016-05-29 13:09:19 +0300

    introduce (in/de)crement, opposite, left, right for Point

--- src/core/point.ml
+++ src/core/point.ml
@@ -4,3 +4,43 @@ type t = | OOOOOO
          | OOO
          | OO
          | O
+
+let increment =   
+  function | OOOOOO -> O
+           | OOOOO -> OOOOOO
+           | OOOO -> OOOOO
+           | OOO -> OOOO
+           | OO -> OOO
+           | O -> OO
+
+let decrement = 
+  function | OOOOOO -> OOOOO
+           | OOOOO -> OOOO
+           | OOOO -> OOO
+           | OOO -> OO
+           | OO -> O
+           | O -> OOOOOO
+
+let opposite =
+  function | OOOOOO -> O
+           | OOOOO -> OO
+           | OOOO -> OOO
+           | OOO -> OOOO
+           | OO -> OOOOO
+           | O -> OOOOOO
+
+let left =
+  function | OOOOOO -> OOO
+           | OOOOO -> OOOOOO
+           | OOOO -> OOOOO
+           | OOO -> OO
+           | OO -> O
+           | O -> OOOO
+
+let right =
+  function | OOOOOO -> OOOOO
+           | OOOOO -> OOOO
+           | OOOO -> O
+           | OOO -> OOOOOO
+           | OO -> OOO
+           | O -> OO
---
+++ src/core/point.mli
@@ -0,0 +1,7 @@
+type t
+
+val increment : t -> t
+val decrement : t -> t
+val opposite  : t -> t
+val left      : t -> t
+val right     : t -> t

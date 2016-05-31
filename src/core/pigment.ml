f494016d30d35ac5fbaee32f7fe6b5deb760818e
Miray Mann <miraymann@gmail.com>
2016-05-31 21:25:28 +0300

    introduce of_char and to_char in Pigment

--- src/core/pigment.ml
+++ src/core/pigment.ml
@@ -4,3 +4,14 @@ let opposite =
   function | Blue -> Gray
            | Gray -> Blue
 
+let to_char = 
+  function | Blue -> '1'
+           | Gray -> '0'
+
+let of_char = 
+  function | '1' -> Blue
+           | '0' -> Gray
+
+ 
+           
+
--- src/core/pigment.mli
+++ src/core/pigment.mli
@@ -2,3 +2,6 @@ type t = | Blue
          | Gray
 
 val opposite : t -> t
+val to_char  : t -> char
+val of_char  : char -> t
+ 

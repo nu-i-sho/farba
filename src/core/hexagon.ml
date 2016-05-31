f258453e80b771b7a2a14274b63da2e44d4365dd
Miray Mann <miray.mann@onnr.co>
2016-06-01 00:56:00 +0300

    replace HexagonSide into Hexagon as Side

--- src/core/hexagon.ml
+++ src/core/hexagon.ml
@@ -2,6 +2,45 @@ type t = { storage : (DNA.t option) array array;
              index : int * int;
          }
 
+module Side = struct
+  type t = | Up
+           | LeftUp
+           | RightUp
+           | Down
+           | LeftDown
+           | RightDown
+
+  let opposite = 
+    function | Up        -> Down
+             | LeftUp    -> RightDown
+             | RightUp   -> LeftDown
+             | Down      -> Up
+             | LeftDown  -> RightUp
+             | RightDown -> LeftUp
+
+  let left_of =
+    function | Up        -> LeftUp
+             | LeftUp    -> LeftDown
+             | LeftDown  -> Down
+             | Down      -> RightDown
+             | RightDown -> RightUp
+             | RightUp   -> Up 
+ 
+  let right_of =
+    function | Up        -> RightUp
+             | RightUp   -> RightDown
+             | RightDown -> Down
+             | Down      -> LeftDown
+             | LeftDown  -> LeftUp
+             | LeftUp    -> Up
+
+  let turn ~to':handSide = 
+    match handSide with
+    | HandSide.Left  -> left_of 
+    | HandSide.Right -> right_of
+
+ end 
+
 let value_of hexagon =
   let storage = hexagon.storage in
   let (x, y)  = hexagon.index 
--- src/core/hexagonSide.mli
+++
@@ -1,9 +0,0 @@
-type t = | Up
-         | LeftUp
-         | RightUp
-         | Down
-         | LeftDown
-         | RightDown
-
-val opposite : t -> t
-val turn : to':HandSide.t -> t -> t

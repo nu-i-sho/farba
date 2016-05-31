c142c54df01af3a68e639e1592e188aa0b9bd9cf
Miray Mann <miray.mann@onnr.co>
2016-06-01 00:29:15 +0300

    introduce DNA.Builder

--- src/core/DNA.ml
+++ src/core/DNA.ml
@@ -3,6 +3,31 @@ type t = {   flesh : Gene.OfFlesh.t;
            program : Gene.OfProgram.t list;
          }
 
+module Builder = struct
+  type d = { accumulator : t 
+           } 
+  
+  let make flesh_gene = 
+    let accumulator = 
+      {   flesh = flesh_gene;
+         spirit = [];
+        program = []
+      } in
+    { accumulator 
+    }
+  
+  let with_spirit builder spirit = 
+    { builder.accumulator with spirit
+    }
+
+  let with_program builder program =  
+    { builder.accumulator with program
+    }
+
+  let result builder = 
+    builder.accumulator
+end
+
 let flesh_of x = x.flesh
 let spirit_of x = x.spirit
 let program_of x = x.program

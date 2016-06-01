b1e880afec759ee03341f9c0541bc829bab2fe0f
Miray Mann <miray.mann@onnr.co>
2016-06-02 02:14:16 +0300

    implement turn and replication in DNA + remove DNA.Builder

---
+++ src/core/DNA.mli
@@ -0,0 +1,12 @@
+type t
+
+val kind_of    : t -> Gene.OfFlesh.Kind.t
+val spirit_of  : t -> RNA.t
+val program_of : t -> Gene.OfFlesh.Command.t array
+val cytoplazm  : Pigment.t -> t
+val turn       : HandSide.t -> t -> t
+
+val replicate  : Relationship.t 
+              -> donor:t 
+              -> acceptor:t option 
+              -> t

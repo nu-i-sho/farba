9b6915819102b4d86ce0093157e583aebe71f051
Miray Mann <miraymann@gmail.com>
2016-06-03 00:32:43 +0300

    remove left/right and add Map.OrderedType to DotsOfDice

--- src/core/dotsOfDice.mli
+++ src/core/dotsOfDice.mli
@@ -1,14 +1,13 @@
-type t = | OOOOOO
-         | OOOOO
-         | OOOO
-         | OOO
-         | OO
-         | O
+include Map.OrderedType with 
+	  t = | OOOOOO
+              | OOOOO
+	      | OOOO
+	      | OOO
+	      | OO
+	      | O
 
 val increment : t -> t
 val decrement : t -> t
 val opposite  : t -> t
-val left      : t -> t
-val right     : t -> t
 val min       : t -> t -> t
 val max       : t -> t -> t

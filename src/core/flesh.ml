5fcc4bb9d54a310003f2978b43a7ce614c5e9f95
Miray Mann <miray.mann@onnr.co>
2016-06-02 23:02:17 +0300

    implement replicate for Flesh

--- src/core/flesh.ml
+++ src/core/flesh.ml
@@ -1,3 +1,41 @@
 type t = | Procaryotic of Procaryote.t
          | Eucaryotic of Eucaryote.t
        
+let replicate relationship ~donor:d ~acceptor:a = 
+  let parent, child = 
+    Eucariotes.replicate relationship d 
+  in
+  
+  let open Eucaryotes in 
+  match child with
+  | Cancer -> 
+     (Eucaryotic Cancer), 
+     (Eucaryotic Cancer)
+
+  | Nucleus x -> 
+     match a with 
+     | None -> 
+	(Eucaryotic parent), 
+	(Eucaryotic child)
+
+     | Some (Eucaryotic _) -> 
+	(Eucaryotic parent, 
+	 Eucaryotic Cancer)
+
+     | Some (Procaryotic Clot) -> 
+	(Procaryotic Clot), 
+	(Procaryotic Clot)
+
+     | Some (Procaryotic (Cytoplazm c)) ->
+	let Nucleus o = child in
+	if (Nucleus.pigment_of o) == (Cytoplazm.pigment_of c) 
+	then  
+	  (Eucaryotic parent),
+	  (Eucaryotic Cancer) 
+	else
+	  let child = Celluar.make ~cytoplazm:c 
+				   ~nucleus:o
+	  in
+
+	  (Eucaryotic parent),
+	  (Eucaryotic child) 

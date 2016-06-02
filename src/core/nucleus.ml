a787927036981c7156678828b5ef1978dae3fdeb
Miray Mann <miray.mann@onnr.co>
2016-06-02 20:59:33 +0300

    implement turn for Nucleus

--- src/core/nucleus.ml
+++ src/core/nucleus.ml
@@ -4,3 +4,7 @@ type t = { program : Command.t list;
             spirit : Virus.t;
 	      mode : LifeMode.t
 	 }
+
+let turn side nucleus = 
+  let gaze = HexagonSide.turn nucleus.gaze ~to:side in
+  { nucleus with gaze }

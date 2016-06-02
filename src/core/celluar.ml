9a292b8f8bbdb726a7c2305b476726877e89bcf8
Miray Mann <miraymann@gmail.com>
2016-06-02 21:37:42 +0300

    implement replicate in Celluar

--- src/core/celluar.ml
+++ src/core/celluar.ml
@@ -4,3 +4,12 @@ type t = { cytoplazm : Cytoplazm.t;
 
 let turn side x =
   { x with nucleus = Nucleus.turn side x }
+
+let replicate relationship x = 
+  let parent_nacleus, child = 
+    Nucleus.replicate relationship x.nacleus
+  in
+
+  { x with nucleus = parent_nacleus },
+  child
+

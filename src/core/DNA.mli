5bd88642c5139222379bb7f81178eb21e0ae8201
Miray Mann <miray.mann@onnr.co>
2016-06-02 03:32:28 +0300

    change type of DNA.spirit to RNA.t option

--- src/core/DNA.mli
+++ src/core/DNA.mli
@@ -1,7 +1,7 @@
 type t
 
 val kind_of    : t -> Gene.OfFlesh.Kind.t
-val spirit_of  : t -> RNA.t
+val spirit_of  : t -> RNA.t option
 val program_of : t -> Gene.OfFlesh.Command.t array
 val cytoplazm  : Pigment.t -> t
 val turn       : HandSide.t -> t -> t

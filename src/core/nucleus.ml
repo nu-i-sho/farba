5800531ac5f7c3635a25a355199eafa57ce1d88f
Miray Mann <miraymann@gmail.com>
2016-06-02 21:20:49 +0300

    implement replicate for Nucleus

--- src/core/nucleus.ml
+++ src/core/nucleus.ml
@@ -8,3 +8,15 @@ type t = { program : Command.t list;
 let turn side nucleus = 
   let gaze = HexagonSide.turn nucleus.gaze ~to:side in
   { nucleus with gaze }
+
+let replicate relationship x = 
+  let gaze = HexagonSide.opposite x.gaze in
+  let pigment =
+    let open Relationship in
+    match relationship with
+    | Inverse -> Pigment.opposite x.pigment
+    | Direct  -> x.pigment
+  in
+   
+  { x with spirit = []   },
+  { x with gaze, pigment }

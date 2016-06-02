c2c0b2482507c52632055450fd29ff9d4805ec9c
Miray Mann <miraymann@gmail.com>
2016-06-02 21:06:19 +0300

    implement turn for for Eucaryotes

--- src/core/eucaryotes.ml
+++ src/core/eucaryotes.ml
@@ -1,3 +1,8 @@
 type t = | Nucleus of Nucleus.t
          | Celluar of Celluar.t
 	 | Cancer
+
+let turn side = 
+  function | Cancer    -> Cancer
+           | Nucleus x -> Nucleus (Nucleus.turn side x)
+           | Celluar x -> Celluar (Celluar.turn side x)

70da6b8456cf899588f81a8a6306df19726fa0f9
Miray Mann <miraymann@gmail.com>
2016-06-02 22:04:50 +0300

    implement make in celluar

--- src/core/celluar.ml
+++ src/core/celluar.ml
@@ -2,6 +2,11 @@ type t = { cytoplazm : Cytoplazm.t;
 	     nucleus : Nucleus.t;
 	 }
 
+let make ~cytoplazm:c ~nucleus:n =
+  { cytoplazm = c;
+      nucleus = n
+  }
+
 let turn side x =
   { x with nucleus = Nucleus.turn side x }
 

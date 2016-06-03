eb137cfd8a291681cb3fa4cd912f2a2cf3fb030e
Miray Mann <miraymann@gmail.com>
2016-06-03 05:07:17 +0300

    fix syntax errors in DotsOfDice

--- src/core/dotsOfDice.ml
+++ src/core/dotsOfDice.ml
@@ -5,8 +5,31 @@ type t = | OOOOOO
          | OO
          | O
 
+let of_int =
+  function | 6 -> OOOOOO
+           | 5 -> OOOOO
+	   | 4 -> OOOO
+	   | 3 -> OOO
+	   | 2 -> OO
+	   | 1 -> O
+
+let to_int = 
+  function | OOOOOO -> 6
+           | OOOOO -> 5
+	   | OOOO -> 4
+	   | OOO -> 3
+	   | OO -> 2
+	   | O -> 1
+
 let compare x y= 
   compare (to_int x) (to_int y)
+
+
+let min x y = 
+  of_int (min (to_int x) (to_int y))
+
+let max x y =
+  of_int (max (to_int x) (to_int y))
 
 let increment =   
   function | OOOOOO -> O
@@ -31,25 +54,3 @@ let opposite =
            | OOO -> OOOO
            | OO -> OOOOO
            | O -> OOOOOO
-
-let of_int =
-  function | 6 -> OOOOOO
-           | 5 -> OOOOO
-	   | 4 -> OOOO
-	   | 3 -> OOO
-	   | 2 -> OO
-	   | 1 -> O
-
-let to_int = 
-  function | OOOOOO -> 6
-           | OOOOO -> 5
-	   | OOOO -> 4
-	   | OOO -> 3
-	   | OO -> 2
-	   | O -> 1
-
-let min x y = 
-  of_int (min (to_int a) (to_int b))
-
-let max x y =
-  of_int (max (to_int a) (to_int b))

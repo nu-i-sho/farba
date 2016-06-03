84375d8cb087f853e1b05a07ed31ed2e04552dd5
Miray Mann <miray.mann@onnr.co>
2016-06-03 11:24:31 +0300

    add spirit and with_spirit to Celluar and simplify

--- src/core/celluar.ml
+++ src/core/celluar.ml
@@ -2,27 +2,24 @@ type t = Nucleus.t;
 
 let make x = x
 
-let nucleus_pigment x = 
-   x |> Nucleus.pigment_of
+let nucleus_pigment = 
+  Nucleus.pigment
 
 let cytoplazm_pigment x =
   x |> nucleus_pigment
     |> Pigment.opposite
 
-let is_cancer x =
-  Nucleus.is_cancer x.nucleus
+let spirit =
+  Nucleus.spirit
 
-let turn side x =
-  let nucleus = 
-    x.nucleus |> Nucleus.turn side 
-  in
- 
-  { x with nucleus }
+let with_spirit =
+  Nucleus.with_spirit
 
-let replicate relationship x = 
-  let parent, child = 
-    x.nucleus |> Nucleus.replicate relationship
-  in
+let is_cancer =
+  Nucleus.is_cancer
 
-  { x with nucleus = parent }, 
-  child
+let turn =
+  Nucleus.turn 
+
+let replicate =
+  Nucleus.replicate
--- src/core/celluar.mli
+++ src/core/celluar.mli
@@ -1,8 +1,10 @@
 type t
 
 val make              : Nucleus.t -> t
-let nucleus_pigment   : t -> Pigment.t
+val nucleus_pigment   : t -> Pigment.t
 val cytoplazm_pigment : t -> Pigment.t
+val spirit            : t -> Virus.t
+val with_spirit       : Virus.t -> t -> t 
 val is_cancer         : t -> bool
 val turn              : HandSide.t -> t -> t
 val replicate         : Relationship.t -> t -> (t * Nucleus.t)

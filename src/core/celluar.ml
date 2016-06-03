924091fe3017eac1eb895ea4a78b8ede924d33b7
Miray Mann <miray.mann@onnr.co>
2016-06-03 17:50:42 +0300

    reimplement Celluar insted Nucleus, Celluar, Eucarypte, Procaryote

--- src/core/celluar.ml
+++ src/core/celluar.ml
@@ -1,25 +1,107 @@
-type t = Nucleus.t;
+type t = {   pigment : Pigment.t;
+                gaze : HexagonSide.t;
+           cytoplazm : Pigment.t option;
+             program : Command.t array;
+              spirit : Breadcrumbs.t option;
+	        mode : LifeMode.t;
+	 }
 
-let make x = x
+module Kind = struct
+    type t = | Hels
+	     | Clot
+	     | Cancer
+end
 
-let nucleus_pigment = 
-  Nucleus.pigment
+module State = struct
+    type t = {    pigment : Pigment.t;
+                     gaze : HexagonSide.t;
+                cytoplazm : Pigment.t option;
+	       has_spirit : bool;
+	             kind : Kind.t
+	     }
+end
 
-let cytoplazm_pigment x =
-  x |> nucleus_pigment
-    |> Pigment.opposite
+let first program = {   pigment = Pigment.Blue;
+			   gaze = HexagonSide.Up;
+                      cytoplazm = None;
+                         spirit = Some Breadcrumbs.start;
+                           mode = LifeMode.Run;
+			program;
+		    }
+let kind pigment cytoplazm = 
+  match pigment, cytoplazm with
+  | _          , Some Pigment.Red -> Kind.Clot
+  | Pigment.Red, _                -> Kind.Cancer
+  | _                             -> Kind.Hels
 
-let spirit =
-  Nucleus.spirit
+let state_of {   pigment = p;
+                    gaze = g;
+               cytoplazm = c;
+                 program = _;
+                  spirit = s;
+	            mode = _;
+	     } = 
+  State.({       kind = kind p c;
+	   has_spirit = s != None;	      
+	      pigment = p;
+		 gaze = g;
+	    cytoplazm = c;
+	 })
 
-let with_spirit =
-  Nucleus.with_spirit
+let turn side x =
+  { x with gaze = x.gaze |> HexagonSide.turn side }
 
-let is_cancer =
-  Nucleus.is_cancer
+let replicate relationship x =       
+                                          (* parent: *)
+    { x with spirit = None; 
+               mode = LifeMode.Free
+    },                                    (* child:  *) 
+    { x with    gaze = HexagonSide.opposite x.gaze;
+             pigment = let open Relationship in
+                       match relationship with
+		       | Inverse -> Pigment.opposite x.pigment
+		       | Direct  -> x.pigment
+    }                                         
 
-let turn =
-  Nucleus.turn 
+let to_clot x = 
+  { x with pigment = Pigment.Red;
+         cytoplazm = Some Pigment.Red;
+            spirit = None
+  }
 
-let replicate =
-  Nucleus.replicate
+let replicate_to_celluar 
+      relationship 
+          ~donor:d 
+       ~acceptor:a =
+
+  match (state_of a).kind with
+  | Kind.Clot -> (to_clot d), a
+  | _         -> 
+     let parent, child = replicate relationship d in
+     let child = 
+       { child with cytoplazm = a.cytoplazm;
+		      pigment = Pigment.Red; 
+       }
+     in
+
+     parent, child
+
+let replicate_to_cytoplazm 
+      relationship 
+          ~donor:d 
+       ~acceptor:a =
+
+  let parent, child = replicate relationship d in
+  let cytoplazm = Pigment.of_hels a in
+  let pigment = if cytoplazm == child.pigment then
+		  Pigment.Red else
+		  child.pigment
+  in
+
+  let child = 
+    { child with cytoplazm = Some cytoplazm; 
+		 pigment; 
+    } 
+  in
+  
+  parent, child
--- src/core/celluar.mli
+++ src/core/celluar.mli
@@ -1,10 +1,32 @@
 type t
 
-val make              : Nucleus.t -> t
-val nucleus_pigment   : t -> Pigment.t
-val cytoplazm_pigment : t -> Pigment.t
-val spirit            : t -> Virus.t
-val with_spirit       : Virus.t -> t -> t 
-val is_cancer         : t -> bool
-val turn              : HandSide.t -> t -> t
-val replicate         : Relationship.t -> t -> (t * Nucleus.t)
+module Kind : sig
+    type t = | Hels
+	     | Clot
+	     | Cancer
+end
+
+module State : sig
+    type t = 
+      private {    pigment : Pigment.t;
+                      gaze : HexagonSide.t;
+                 cytoplazm : Pigment.t option;
+	        has_spirit : bool;
+	              kind : Kind.t
+	      }
+  end
+
+val first     : Command.t array -> t
+val state_of  : t -> State.t 
+val turn      : HandSide.t -> t -> t
+val replicate : Relationship.t -> t -> (t * t)
+
+val replicate_to_cytoplazm : Relationship.t
+	                  -> donor : t 
+                          -> acceptor : HelsPigment.t
+			  -> (t * t)
+
+val replicate_to_celluar : Relationship.t
+	                -> donor : t 
+                        -> acceptor : t
+	                -> (t * t)

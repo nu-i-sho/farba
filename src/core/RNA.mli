3636ce5f31938b988c675768e948b415f354d153
Miray Mann <miraymann@gmail.com>
2016-06-03 04:24:42 +0300

    remove RNA and DNA

--- src/core/RNA.mli
+++
@@ -1,8 +0,0 @@
-type t
-
-val empty     : t
-val kind_of   : t -> Gene.OfSpirit.Kind.t
-val energy_of : t -> Gene.OfSpirit.Breadcrumb.t list
-val step      : t -> t
-val active_breadcrumb_of
-              : t -> Gene.OfSpirit.Breadcrumb.t

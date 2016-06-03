4e6e1710d226c29a6aeee4e975548a0a9861b22e
Miray Mann <miraymann@gmail.com>
2016-06-03 11:14:57 +0300

    add spirit and with spirit to Nucleus

--- src/core/nucleus.mli
+++ src/core/nucleus.mli
@@ -1,6 +1,8 @@
 type t
 
-val pigment   : t -> Pigment.t
-val is_cancer : t -> bool
-val turn      : HandSide.t -> t -> t
-val replicate : Relationship.t -> t -> (t * t)
+val pigment     : t -> Pigment.t
+let spirit      : t -> Virus.t
+let with_spirit : Virus.t -> t -> t
+val is_cancer   : t -> bool
+val turn        : HandSide.t -> t -> t
+val replicate   : Relationship.t -> t -> (t * t)

85192297636afcf5cdc80dbb65a71ccfa5f15dd1
Miray Mann <miraymann@gmail.com>
2016-06-03 10:26:15 +0300

    add clot support Cytoplazm

--- src/core/cytoplazm.ml
+++ src/core/cytoplazm.ml
@@ -2,3 +2,4 @@ type t = Pigment.t
 
 let make pigment = pigment
 let pigment_of x = x
+let is_clot = (==) Pigment.Red  
--- src/core/cytoplazm.mli
+++ src/core/cytoplazm.mli
@@ -2,3 +2,4 @@ type t
 
 val make       : Pigment.t -> t
 val pigment_of : t -> Pigment.t
+val is_clot    : t -> bool

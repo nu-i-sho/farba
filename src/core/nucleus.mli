976a12dac926442103b3af190d68154560898657
Miray Mann <miraymann@gmail.com>
2016-06-03 09:39:49 +0300

    add cancer supporting to Nucleus

--- src/core/nucleus.mli
+++ src/core/nucleus.mli
@@ -1,5 +1,6 @@
 type t
 
 val pigment_of : t -> Pigment.t
-val turn : HandSide.t -> t -> t
-val replicate : Relationship.t -> t -> (t * t)
+val is_cancer  : t -> bool
+val turn       : HandSide.t -> t -> t
+val replicate  : Relationship.t -> t -> (t * t)

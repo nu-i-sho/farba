85ea016955b99863197b46524d5e1721fc70e314
Miray Mann <miray.mann@onnr.co>
2016-06-03 06:19:44 +0300

    fix syntax errors in Hexagon

--- src/core/hexagon.mli
+++ src/core/hexagon.mli
@@ -1,7 +1,7 @@
 type t
 
 val make          : Set.t -> t 
-val value_of      : t -> DNA.t option
+val value_of      : t -> Flesh.t option
 val neighbor_from : HexagonSide.t -> t -> t
 
-val set_value     : DNA.t option -> t -> t
+val set_value     : Flesh.t option -> t -> t

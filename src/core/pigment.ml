e4fd5ede317405f4c0edbf2fcb05ef536cafcd49
Miray Mann <miraymann@gmail.com>
2016-06-03 09:35:31 +0300

    add Red pigment

--- src/core/pigment.ml
+++ src/core/pigment.ml
@@ -1,17 +1,18 @@
-type t = Blue | Gray
+type t = | Blue
+         | Gray
+	 | Red
 
 let opposite = 
   function | Blue -> Gray
            | Gray -> Blue
+	   | Red  -> Red
 
 let to_char = 
   function | Blue -> '1'
            | Gray -> '0'
+	   | Red  -> '2'
 
 let of_char = 
   function | '1' -> Blue
            | '0' -> Gray
-
- 
-           
-
+	   | '2' -> Red

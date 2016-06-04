2a2219dada64cc46604ab79de19d48eb35ef5d6c
Miray Mann <miraymann@gmail.com>
2016-06-04 05:04:02 +0300

    do Breadcrumbs observable

--- src/core/breadcrumbs.ml
+++ src/core/breadcrumbs.ml
@@ -1,14 +1,36 @@
-module CrumbMap = Map.Make (DotsOfDice)
-module IntMap = Map.Make (ComparableInt)
+module Dots = DotsOfDice 
+module CrumbMap = Map.Make (Dots)
+module PlaceMap = Map.Make (ComparableInt)
+
+module Event = struct
+    type t = | New   (Dots.t * int)
+             | Move  (Dots.t * int * int) 
+             | Split (Dots.t * Dots.t * Dots.t * int)
+	     | Marge (Dots.t * Dots.t * Dots.t * int)
+end
+
 
 type t = { crumbs : int CrumbMap.t;
-	   places : DotsOfDice.t IntMap.t;
+	   places : Dots.t PlaceMap.t;
+	     next : (Event.t -> unit) option 
          }
 
-let start = 
-  { places = IntMap.singleton 0 DotsOfDice.O;
-    crumbs = CrumbMap.singleton DotsOfDice.O 0;
+let start =
+  { places = PlaceMap.singleton 0 Dots.O;
+    crumbs = CrumbMap.singleton Dots.O 0;
+      next = None;
   }
+
+let next event x =
+  let () = match x.on_event with
+           | Some on -> on !event
+           | None    -> ()
+  in
+  x
+
+let starto observer = 
+  { start with next = Some observer } 
+  |> next Event.(New (Dots.O, 0))
 
 let last x = CrumbMap.max_binding x.crumbs 
 let last_place x = snd (last x)  
@@ -20,34 +42,38 @@ let count x =
 let length x = 
   (last_place x) + 1
 
-let increment x =
-  let crumb, place = last x in
-  let place' = place + 1 in
-  { crumbs = x.crumbs |> CrumbMap.remove crumb
-                      |> CrumbMap.add crumb place';
-    places = x.places |> IntMap.remove place
-		      |> IntMap.add place' crumb;
+let remove c p x = 
+  { x with crumbs = x.crumbs |> CrumbMap.remove c;
+           places = x.places |> PlaceMap.remove p;
   }
+
+let add c p x = 
+  { x with crumbs = x.crumbs |> CrumbMap.add c p;
+           places = x.places |> PlaceMap.add p c;
+  }
+
+let increment x =
+  let c, p = last x in
+  x |> next Event.(Move (c, p, p + 1))
+    |> remove c p
+    |> add c (p + 1)
 
 let decrement x =
-   let crumb, place = last x in
-   let y = { crumbs = x.crumbs |> CrumbMap.remove crumb;
-             places = x.places |> IntMap.remove place;
-	   }
+   let rec (c, p), c', p' = 
+     (last x), (Dots.increment c), (p - 1)
    in
-   
-   let place' = place - 1 in
-   if x.places |> IntMap.mem place' then y else
-     { crumbs = y.crumbs |> CrumbMap.add crumb place';
-       places = y.places |> IntMap.add place' crumb;
-     }
 
-let multiply x =
-  let crumb , place  = last x in
-  let crumb', place' = 
-    (DotsOfDice.increment crumb), (place + 1)
+   x |> remove c p
+     |> next Event.(Move (c, p, p'))
+     |> if PlaceMap.mem p' x.places then 
+	  next Event.(Split (c, c', c', p')) else 
+	  add c p'
+
+let split x =
+  let rec (c, p), c', p' = 
+    (last x), (Dots.increment c), (p + 1) 
   in
 
-  { crumbs = x.crumbs |> CrumbMap.add crumb' place';
-    places = x.places |> IntMap.add place' crumb';
-  }
+  x |> next Event.(Split (c, c, c', p))
+    |> next Event.(Split (c', p, p'))
+    |> add c' p'
--- src/core/breadcrumbs.mli
+++ src/core/breadcrumbs.mli
@@ -1,6 +1,14 @@
+module Event : sig
+    type t = private | New   (Dots.t * int)
+		     | Move  (Dots.t * int * int) 
+		     | Split (Dots.t * Dots.t * Dots.t * int)
+		     | Marge (Dots.t * Dots.t * Dots.t * int)
+end
+
 type t 
 
 val start      : t
+val starto     : (BreadcrumbsEvent.t -> unit) -> t
 val last       : t -> DotsOfDice.t * int
 val last_crumb : t -> DotsOfDice.t
 val last_place : t -> int
@@ -8,4 +16,4 @@ val count      : t -> int
 val length     : t -> int
 val increment  : t -> t
 val decrement  : t -> t
-val multiply   : t -> t
+val split      : t -> t

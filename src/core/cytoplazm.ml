98e45bd201bce40e8986a0a5a0b305d06ca96cb1
Miray Mann <miray.mann@onnr.co>
2016-06-04 05:18:18 +0300

    remove Cytoplazm

--- src/core/cytoplazm.ml
+++
@@ -1,6 +0,0 @@
-type t = Pigment.t
-
-let make pigment = pigment
-let clot = Pigment.Red
-let pigment x = x
-let is_clot = (==) clot  
--- src/core/cytoplazm.mli
+++
@@ -1,6 +0,0 @@
-type t
-
-val make    : Pigment.t -> t
-val clot    : t
-val pigment : t -> Pigment.t
-val is_clot : t -> bool

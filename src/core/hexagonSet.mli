3858aea021f2fdb7982609c9117fdbd913fe480b
Miray Mann <miray.mann@onnr.co>
2016-06-04 05:21:26 +0300

    implement HexagonSet with intrnal Index and better code style

--- src/core/hexagonSet.mli
+++ src/core/hexagonSet.mli
@@ -1,3 +1,13 @@
-type t = Flesh.t option array array
+module Index : sig
+    type t
 
+    let zero : t
+    let move : HexagonSide.t -> t -> t
+  end
+		 
+type element_t = Flesh.t option
+type t
+
+val get  : Index.t -> t -> element_t
+val set  : Index.t -> element_t -> t -> unit
 val read : string -> t

ab8a98ca9cb3291232a3be72ff800ddb3b7089c5
Miray Mann <miraymann@gmail.com>
2016-06-04 22:05:59 +0300

    introduce Set

---
+++ src/core/set.mli
@@ -0,0 +1,20 @@
+module Index : sig
+    type t
+
+    val zero : t
+    val move : HexagonSide.t -> t ->t
+  end
+
+module Value : sig
+    type t = | Cytoplazm of HelsPigment.t
+             | Celluar of Celluar.t
+             | Empty
+  end
+
+type t
+     
+val width  : t -> int
+val height : t -> int
+val get    : Index.t -> t -> Element.t
+val set    : Index.t -> Value.t -> t -> Element.t
+val read   : string -> t

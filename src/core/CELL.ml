18812fccf973ac4bd8d463d44b0cf0af39c56bb9
Miray Mann <miray.mann@onnr.co>
2016-06-06 02:18:04 +0300

    introduce CELL.Data and it using in CELL.T

--- src/core/CELL.ml
+++ src/core/CELL.ml
@@ -1,10 +1,29 @@
+module Data = struct
+    module Make (Cell : T'.T) = struct
+
+        module State = struct
+            type t = {  body : Protocell.t;
+                       index : Set.Index.t
+                     }
+          end
+
+        module ReplicationResult = struct
+            type t = | SelfClotted of Cell.t'
+                     | Replicated of Cell.t'
+                     | ReplicatedOut
+         end
+     end
+  end
+
 module type T = sig
     type t
-    type rep_res_t
+
+    include module type of Data.Make(struct type t' = t end)
+    val state_of : t -> State.t
 
     val turn : HandSide.t -> t -> t
 
     val replicate : relationship : Relationship.t 
-		 -> donor : t
-		 -> rep_res_t
+                 -> donor : t
+                 -> ReplicationResult.t
   end

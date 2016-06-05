34a394cd32517860773d77cea92311ffb7c8ebc7
Miray Mann <miraymann@gmail.com>
2016-06-05 19:39:01 +0300

    rename replication_result_t to rep_res_t in CELL.T

--- src/core/CELL.ml
+++ src/core/CELL.ml
@@ -1,10 +1,10 @@
 module type T = sig
     type t
-    type replication_result_t
+    type rep_res_t
 
     val turn : HandSide.t -> t -> t
 
     val replicate : relationship : Relationship.t 
 		 -> donor : t
-		 -> replication_result_t
+		 -> rep_res_t
   end

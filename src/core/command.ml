396b50e85cdce7d3e2d3ea84f9ea255400e20dac
Miray Mann <miraymann@gmail.com>
2016-06-08 00:35:24 +0300

    simplify Command

--- src/core/command.ml
+++ src/core/command.ml
@@ -1,15 +1,5 @@
-module ForAct = struct
-    type t = | Turn of HandSide.t
-	     | Replicate of Relationship.t
-	     | Call of DotsOfDice.t 
-  end
-
-module ForMark = struct
-    type t = | Declare of DotsOfDice.t
-	     | Begin
-	     | End
-  end
-
-type t = | Act of ForAct.t
-         | Mark of ForMark.t
-
+type t = | Turn of HandSide.t
+	 | Replicate of Relationship.t
+	 | Call of DotsOfDice.t 
+	 | Declare of DotsOfDice.t
+	 | End

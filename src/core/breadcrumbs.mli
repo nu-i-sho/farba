e48ac5a043fe2aa2d00ed2fc087341a524553b49
Miray Mann <miray.mann@onnr.co>
2016-06-08 03:33:32 +0300

    romove observing possibility from, and add is_empty to Breadcrumbs

--- src/core/breadcrumbs.mli
+++ src/core/breadcrumbs.mli
@@ -1,18 +1,9 @@
-module Dots = DotsOfDice
-
-module Event : sig
-    type t = private | Create of Dots.t * int
-                     | Move   of Dots.t * int * int 
-                     | Split  of Dots.t * Dots.t * Dots.t * int
-		     | Marge  of Dots.t * Dots.t * Dots.t * int
-end
-
 type t 
 
 val start      : t
-val starto     : observer:(Event.t -> unit) -> t
-val last       : t -> Dots.t
+val last       : t -> DotsOfDice.t
 val last_place : t -> int
+val is_empty   : t -> bool
 val count      : t -> int
 val length     : t -> int
 val increment  : t -> t

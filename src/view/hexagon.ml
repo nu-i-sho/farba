9929103f9171ac4bfba5b25d77380fcd3fd1e552
Miray Mann <miray.mann@onnr.co>
2016-06-11 23:04:23 +0300

    introduce Hexagon with more rich interface

--- src/view/hexagon.ml
+++ src/view/hexagon.ml
@@ -1,27 +1,49 @@
 type t = 
-  { _r : float; (* -- radius of inside circle *)
-    _R : float  (* -- radius of outside circle *)
+  { internal_radius : float;
+    external_radius : float
   }
 
-let make t =
-  (* t is length of side of regular hexagon *)
-  let t = float t in
-  { _r = 0.86602540378 *. t;
-    _R = t
+let make side =
+  let side = float side in
+  { internal_radius = side *. 0.86602540378;
+    external_radius = side
   }
 
-module Coord = struct
-    let round x =
-      let fractional, integral = modf x in
-      (int_of_float integral) 
-      + (if fractional > 0.5 then 
-	   1 else 
-	   0)
+let round x =
+  let fractional, integral = modf x in
+  (int_of_float integral) 
+  + (if fractional > 0.5 then 
+       1 else 
+       0)
 
-    let of_index (x, y) hexagon =
-      let _r, _R = hexagon._r, hexagon._R in
-      let y = y * 2 + lnot(x mod 2) in
-      (round (_R +. (float x) *. (_R +. _r /. 2.0))), 
-      (round (((float y) +. 1.0) *. _r))
-	       
-end
+
+let internal_radius_f o = o.internal_radius
+let external_radius_f o = o.external_radius
+let internal_radius o = round (internal_radius_f o)
+let external_radius o = round (external_radius_f o)
+let side = external_radius
+
+let center_coord_f (x, y) o =
+  let r  = internal_radius_f o in
+  let r' = external_radius_f o in
+  let y  = y * 2 + lnot(x mod 2) in
+  (r' +. (float x) *. r' *. 1.5), 
+  (((float y) +. 1.0) *. r)
+
+let center_coord i o =
+  let x, y = center_coord_f i o in
+  (round x), 
+  (round y)
+
+let angles_coords i o =
+  let x, y = center_coord_f i o in
+  let r  = internal_radius_f o in
+  let r' = external_radius_f o in
+
+  [| round (x +. r' /. 2.0), round (y -. r);
+     round (x +. r')       , round  y;
+     round (x +. r' /. 2.0), round (y +. r);
+     round (x -. r' /. 2.0), round (y +. r);
+     round (x -. r')       , round  y;
+     round (x -. r' /. 2.0), round (y -. r); 
+  |]

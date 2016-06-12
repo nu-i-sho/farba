f52608b94d849d6c03d1bed2a95c8d72041e8b93
Miray Mann <miraymann@gmail.com>
2016-06-12 19:09:27 +0300

    add Nucleus signature and stub

---
+++ src/view/nucleus.ml
@@ -0,0 +1,23 @@
+type t = {     hexagon : Hexagon.t;
+                radius : float;
+	    eye_radius : float
+	 }
+
+let make h =
+  let r = Hexagon.internal_radius_f h in
+  {    hexagon = h; 
+        radius = r *. 0.8;
+    eye_radius = r *. 0.1
+  }
+
+let radius o = 
+  Int.round o.radius
+ 
+let eye_radius o = 
+  Int.round o.eye_radius
+
+let eyes_coords _ _ _ = 
+  (0, 0)
+
+let cancer_lines_coords _ _ _ = 
+  ((0, 0), (0, 0))

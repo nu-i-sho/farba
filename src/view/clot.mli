3f30af6909a7269aa9da52124906785c9f11ffae
Miray Mann <miraymann@gmail.com>
2016-06-12 20:34:58 +0300

    change Clot.eyes_coords result type

--- src/view/clot.mli
+++ src/view/clot.mli
@@ -3,4 +3,5 @@ type t
 val eyes_coords : Index.t 
 	       -> Side.t 
 	       -> t 
-	       -> ((int * int) * (int * int))
+	       -> (((int * int) * (int * int)) * 
+                   ((int * int) * (int * int)))

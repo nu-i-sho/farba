a5497383463b5811fa84853f71940f087fa42cc2
Miray Mann <miraymann@gmail.com>
2016-06-12 03:32:11 +0300

    remove files commitd by mistake

--- src/shared/SET.ml
+++
@@ -1,8 +0,0 @@
-module type T = sig
-    type t
-     
-    val width  : t -> int
-    val height : t -> int
-    val get    : Index.t -> t -> Item.t
-    val set    : Index.t -> Item.t -> t -> unit
-end

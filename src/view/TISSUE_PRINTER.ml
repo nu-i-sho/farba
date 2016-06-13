2d142b91cfe374650cc63e0bca13550112bb54bf
Miray Mann <miraymann@gmail.com>
2016-06-13 08:38:32 +0300

    remove unnecessary Index.t parameter from TISSUE_PRINTER.T functions signatures

--- src/view/TISSUE_PRINTER.ml
+++ src/view/TISSUE_PRINTER.ml
@@ -1,13 +1,14 @@
 module type T = sig
     type t
 
+    val zero         : t
     val set_index    : Index.t -> t -> t
     val set_color    : Graphics.color -> t -> t
-    val draw_hexagon : Index.t -> t -> t
-    val fill_hexagon : Index.t -> t -> t
-    val draw_nucleus : Index.t -> t -> t
-    val fill_nucleus : Index.t -> t -> t
-    val draw_eyes    : Index.t -> Eyes.t -> t -> t
-    val fill_eyes    : Index.t -> t -> t
+    val draw_hexagon : t -> t
+    val fill_hexagon : t -> t
+    val draw_nucleus : t -> t
+    val fill_nucleus : t -> t
+    val draw_eyes    : Eyes.t -> t -> t
+    val fill_eyes    : t -> t
 
   end

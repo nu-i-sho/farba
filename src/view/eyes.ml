470ce9d3140cc12846bfa40de3146c5e2e33da91
Miray Mann <miraymann@gmail.com>
2016-06-14 01:25:58 +0300

    add of_cell to Eyes

--- src/view/eyes.ml
+++ src/view/eyes.ml
@@ -2,3 +2,9 @@ type t = | Hels of Side.t
          | Cancer of Side.t
          | Clot of Side.t
          | Cytoplasm
+
+let of_cell c =
+  match Protocell.kind_of c with
+  | CellKind.Hels   -> Hels c.gaze
+  | CellKind.Clot   -> Clot c.gaze
+  | CellKind.Cancer -> Cancer c.gaze

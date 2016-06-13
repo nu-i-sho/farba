bbb5d306337012eed1959b739ad96cb1215be789
Miray Mann <miray.mann@onnr.co>
2016-06-13 04:44:13 +0300

    introduce TissueScale signature and partitionaly implemented structure

---
+++ src/view/tissueScale.ml
@@ -0,0 +1,47 @@
+module Make (O : sig val hexagon_side : int end) = struct
+   
+    module Hexagon = struct
+
+	let side = 
+	  O.hexagon_side
+
+	let external_radius = 
+	  float side
+
+	let internal_radius = 
+	  external_radius *. 0.8
+
+	let agles =
+	  let e  = Int.round external_radius in
+	  let i  = Int.round internal_radius in
+	  let e' = Int.round (external_radius *. 0.5) in
+
+	  [| +e', -i;
+             +e ,  0;
+             +e', +i;
+             -e', +i;
+             -e ,  0;
+             -e', -i; 
+	  |]
+      end
+
+    module Cytoplazm = struct
+	let eyes_radius = 0
+	let eyes_coords = (0, 0), (0, 0)
+      end
+
+    module Nucleus = struct
+	let radius = 0
+	let eyes_radius = 0
+	let eyes_coords = (0, 0), (0, 0)
+      end
+
+    module Cancer = struct
+	let eyes_coords = (0, 0), (0, 0)
+      end
+
+    module Clot = struct
+	let eyes_coords = (0, 0), (0, 0)
+      end
+		       
+  end

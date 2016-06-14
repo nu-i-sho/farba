b37d5e56536cd2b5731a654bc16ae24e2dfa0b80
Miray Mann <miraymann@gmail.com>
2016-06-15 01:21:59 +0300

    update Breadcrumbs signature with BREADCRUMBS including

--- src/core/breadcrumbs.mli
+++ src/core/breadcrumbs.mli
@@ -1,11 +1,1 @@
-type t 
-
-val start      : t
-val last       : t -> DotsOfDice.t
-val last_place : t -> int
-val is_empty   : t -> bool
-val count      : t -> int
-val length     : t -> int
-val increment  : t -> t
-val decrement  : t -> t
-val split      : t -> t
+include BREADCRUMBS.T

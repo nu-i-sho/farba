e08ba9bd75653e30af9c5fd1b2b76d72bbd416ca
Miray Mann <miray.mann@onnr.co>
2016-06-15 01:17:38 +0300

    introduce TissueCell.Make

--- src/core/tissueCell.mli
+++ src/core/tissueCell.mli
@@ -1,7 +1,2 @@
-type t
-
-val first : Tissue.t -> Index.t -> t option
-val kind_of : t -> CellKind.t
-val is_out : t -> bool
-val turn : Hand.t -> t -> t
-val replicate : Relationship.t -> t -> t
+module Make (Tissue : TISSUE.T)
+                    : TISSUE_CELL.T

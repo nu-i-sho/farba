1307ed30c27b92cf94407d8477725ce4279bf47f
Miray Mann <miray.mann@onnr.co>
2016-06-14 18:09:40 +0300

    introduce DefaultColorSheme

--- src/view/TISSUE_COLOR_SHEME.ml
+++ src/view/TISSUE_COLOR_SHEME.ml
@@ -2,9 +2,9 @@ module type T = sig
 
     val blue_pigment : int
     val gray_pigment : int
-    val cancer : int
-    val clot : int
-    val virus : int
-    val line : int
+    val cancer       : int
+    val clot         : int
+    val virus        : int
+    val line         : int
 
   end

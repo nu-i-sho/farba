dc9b28376ec91dc03d6d3dccc750a2e71a95ea9d
Kitanna <kita.mann.888@gmail.com>
2016-06-16 00:14:51 +0300

    implement Img._20x20_.DOtsOfDice.(O, OO, OOO, OOOO)

--- src/view/img/20x20/dots/OO.ml
+++ src/view/img/20x20/dots/OO.ml
@@ -1,12 +1,12 @@
 let prototype =
   lazy ([| 
        (*    01234567890123456789*) 
-       (*0*)"HHHHHHHHHHHHHHHHHHHH";
-       (*1*)"HHHHHHHHHHHHHHHHHHHH";
-       (*2*)"HHHHHHHHHHHHHHHHHHHH";
-       (*3*)"HHHHHHHHHHHHHHHHHHHH";
-       (*4*)"HHHHHHHHHHHHHHHHHHHH";
-       (*5*)"HHHHHHHHHHHHHHHHHHHH";
+       (*0*)"  HHHHHHHHHHHHHHHH  ";
+       (*1*)" HHHHHHHHHHHHHHHHHH ";
+       (*2*)"HHHHHHHHHHHHH----HHH";
+       (*3*)"HHHHHHHHHHHHH----HHH";
+       (*4*)"HHHHHHHHHHHHH----HHH";
+       (*5*)"HHHHHHHHHHHHH----HHH";
        (*6*)"HHHHHHHHHHHHHHHHHHHH";
        (*7*)"HHHHHHHHHHHHHHHHHHHH";
        (*8*)"HHHHHHHHHHHHHHHHHHHH";
@@ -14,12 +14,12 @@ let prototype =
        (*0*)"HHHHHHHHHHHHHHHHHHHH";
        (*1*)"HHHHHHHHHHHHHHHHHHHH";
        (*2*)"HHHHHHHHHHHHHHHHHHHH";
-       (*3*)"HHHHHHHHHHHHHHHHHHHH";
-       (*4*)"HHHHHHHHHHHHHHHHHHHH";
-       (*5*)"HHHHHHHHHHHHHHHHHHHH";
-       (*6*)"HHHHHHHHHHHHHHHHHHHH";
+       (*3*)"HHH----HHHHHHHHHHHHH";
+       (*4*)"HHH----HHHHHHHHHHHHH";
+       (*5*)"HHH----HHHHHHHHHHHHH";
+       (*6*)"HHH----HHHHHHHHHHHHH";
        (*7*)"HHHHHHHHHHHHHHHHHHHH";
-       (*8*)"HHHHHHHHHHHHHHHHHHHH";
-       (*9*)"HHHHHHHHHHHHHHHHHHHH";
+       (*8*)" HHHHHHHHHHHHHHHHHH ";
+       (*9*)"  HHHHHHHHHHHHHHHH  ";
 
        |])

d7aab9ec62e4d3fe026af26ec6194471d73b8639
Miray Mann <miray.mann@qconnr.com>
2016-06-16 01:38:08 +0300

    introduce IMG_PROTOTYPE and rename Img to ImgPrototype

---
+++ src/view/IMG_PROTOTYPE.ml
@@ -0,0 +1,31 @@
+module type T = sig
+    val matrix : string array Lazy.t
+end
+
+module DOTS_OF_DICE = struct
+    module type T = sig
+	module OOOOOO : T
+	module OOOOO : T
+	module OOOO : T
+	module OOO : T
+	module OO : T
+	module O : T
+      end
+  end
+
+module ACT = struct
+    module type T = sig
+	module TurnLeft : T
+	module TurnRight : T
+	module ReplicateDirect : T
+	module ReplicateInvarese : T
+      end
+  end
+
+module COMMAND = struct
+    module type T = sig
+	module DotsOfDice : DOTS_OF_DICE.T
+	module Act : ACT.T
+	module End : T
+      end
+  end

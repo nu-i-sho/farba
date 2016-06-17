292901bfd8e625242fdcc71fdeb4927014c41ab6
Miray Mann <miray.mann@qconnr.com>
2016-06-17 13:58:30 +0300

    addpointer to parameterslist of ProgramPrinter (..)(..)(..).print

--- src/view/programPrinter.ml
+++ src/view/programPrinter.ml
@@ -2,10 +2,8 @@ module Make     (Canvas : CANVAS.T)
             (CommandImg : IMG.COMMAND.T)
                (Pointer : PROGRAM_POINTER.T) = struct
 
-    let print program =
-      let length  = Program.length program in
-      let pointer = Pointer.make length in
-      for i = 0 to length do
+    let print program pointer =
+      for i = 0 to Program.length program do
 	Canvas.draw_image (program |> Program.get i
                                    |> CommandImg.get)
 			  (pointer |> Pointer.get i)
--- src/view/programPrinter.mli
+++ src/view/programPrinter.mli
@@ -2,6 +2,6 @@ module Make (Canvas : CANVAS.T)
         (CommandImg : IMG.COMMAND.T)
            (Pointer : PROGRAM_POINTER.T) : sig
 
-    val print : Program.t -> unit
+    val print : Program.t -> Pointer.t -> unit
 
   end

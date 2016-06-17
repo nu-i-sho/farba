939ecfa233de4da4c1e4d09639988feedb940b6d
Miray Mann <miray.mann@onnr.co>
2016-06-17 15:02:17 +0300

    implement programPointer.ml

--- src/view/programPointer.ml
+++ src/view/programPointer.ml
@@ -1,4 +1,19 @@
 type t = unit
 
 let make count = () 
-let get i o = (0, 0)
+let get i _ = 
+  let y = (((i / 10) * 8)  
+        + (((i mod 10) / 5) * 4) 
+        + (((i mod 5) / 4) * 2)
+        + 1)
+        * 27
+  in
+
+  let x = ( match i mod 10 with
+            |0|8|9 -> 0
+            |1|7   -> 1
+            |2|6   -> 2
+            |3|4|5 -> 3 ) * 27
+  in 
+
+  x, y

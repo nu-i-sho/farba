f5e1eeeecf6eda378b91d657dd1347ffa73c84ee
Miray Mann <miray.mann@onnr.co>
2016-06-17 06:25:34 +0300

    implement list based Breadcrumbs

--- src/core/breadcrumbs.ml
+++ src/core/breadcrumbs.ml
@@ -1,33 +1,21 @@
-type t = (DotsOfDice.t * int) Stack.t
+module Dots = DotsOfDice
 
-let start =
-  let o  = Stack.create () in
-  let () = Stack.push (DotsOfDice.O, 0) o in
-  o
+type t = (Dots.t * int) list
 
-let last_pair    = Stack.pop 
-let last       o = fst (last_pair o)
+let start        = [Dots.O, 0]
+let last_pair    = List.hd 
+let last o       = fst (last_pair o)
 let last_place o = snd (last_pair o)  
-let count        = Stack.length
+let count        = List.length
 let length     o = (last_place o) + 1
-let is_empty     = Stack.is_empty
+let is_empty     = (=) []
 
-let increment o =
-  let (crumb, place) = Stack.pop o in
-  let () = Stack.push (crumb, place + 1) o in
-  o
+let increment ((a, x) :: t) = (a, x + 1) :: t
 
-let decrement o =
-  let crumb_1, place_1 = Stack.pop o in
-  let crumb_0, place_0 = Stack.top o in
-  let place_1 = place_1 - 1 in
-  if place_1 = place_0 then o else
-    let () = Stack.push (crumb_1, place_1) o in
-    o
+let decrement = 
+  function | (_, x) :: (b, y) :: t 
+		when (x - 1) = y -> (b, y) :: t
+           | (a, x) :: t         -> (a, x - 1) :: t
 
-let split o =
-  let crumb_0, place_0 = Stack.top o in
-  let crumb_1 = crumb_0 |> DotsOfDice.increment in
-  let place_1 = place_0 + 1 in
-  let () = Stack.push (crumb_1, place_1) o in
-  o
+let split ((a, x) :: t) = 
+  (Dots.increment a, x + 1) :: (a, x) :: t

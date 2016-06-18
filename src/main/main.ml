f0f5b71a19b10ce80898638e923108ccbb51ed5e
Miray Mann <miray.mann@onnr.co>
2016-06-18 19:46:43 +0300

    implemant program delaying

--- src/main/main.ml
+++ src/main/main.ml
@@ -25,23 +25,17 @@ let () = begin
 		      (TissueCell) 
     in
 
-    let () = print_string "-1-" in
     let colony = Colony.load [ "10101";
 	  		       "11101";
 			       "01101";
 			       "00101"
                              ]
     in
-    let () = print_string "-2-" in
     let Some cell = TissueCell.first colony (0, 0) in
-    let () = print_string "-3-" in
-    let program = Program.load "AB123ABAFE16572220" in
-    let () = print_string "-4-" in
+    let program = Program.load "011021011222212300" in
     let pointer = View.ProgramPointer.make 
 		    (Program.length program) in
-    let () = print_string "-5-" in
     let () = ProgramPrinter.print program pointer in
-    let () = print_string "-6-" in
     let breadcrumbs = Breadcrumbs.make ~pointer
 		       ~breadcrumbs:Core.Breadcrumbs.start in
     let () = print_string "-7-" in
@@ -49,16 +43,26 @@ let () = begin
                           ~infected:cell 
                        ~breadcrumbs
     in
-    let rec run o = 
-      let () = Unix.sleep 1 in
-      match Virus.next o with
-      | Some o -> run o
+    let () = print_string "-8-" in
+    let rec main o frame_time =
+      match o with
+      | Some o -> 
+	 let () = print_string "*" in
+	 let rec delay () =
+	   let duration = frame_time +. 2.225 -. 
+			    Unix.gettimeofday () 
+	   in
+	   if duration > 0.0 then
+	     try
+               Thread.delay duration
+	     with | Unix.Unix_error (Unix.EAGAIN, _, _) 
+		  | Unix.Unix_error (Unix.EINTR, _, _) 
+		    -> delay ()
+	 in
+	 let () = delay () in
+	 let () = print_string "#" in
+	 main (Virus.next o) (Unix.gettimeofday  ())
       | None -> ()
     in
-    let () = Graphics.open_graph "" in
-    run virus
-  end 
-(*with e -> 
-  let trace = Printexc.get_raw_backtrace () in
-  Printexc.print_raw_backtrace stdout trace
- *)
+    main (Some virus) (Unix.gettimeofday  ())
+  end

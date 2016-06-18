8424296955c2b14a4969b860e8a45f3b5e031a65
Miray Mann <miray.mann@onnr.co>
2016-06-18 05:57:59 +0300

    fix out of range in ProgramPrinter

--- src/view/programPrinter.ml
+++ src/view/programPrinter.ml
@@ -3,7 +3,7 @@ module Make     (Canvas : CANVAS.T)
                (Pointer : PROGRAM_POINTER.T) = struct
 
     let print program pointer =
-      for i = 0 to Program.length program do
+      for i = 0 to (Program.length program) - 1 do
 	Canvas.draw_image (program |> Program.get i
                                    |> CommandImg.get)
 			  (pointer |> Pointer.get i)

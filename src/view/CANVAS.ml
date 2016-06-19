efe7e14bd6ca05595343a9199fedcbe134341491
Miray Mann <miray.mann@onnr.co>
2016-06-19 05:26:21 +0300

    remove width height from CANVAS

--- src/view/CANVAS.ml
+++ src/view/CANVAS.ml
@@ -1,14 +1,12 @@
 module type T = sig
     
-    val width          : int
-    val height         : int
     val set_color      : Graphics.color -> unit
     val set_line_width : int -> unit
     val draw_poly      : Point.t array -> unit
     val draw_ellipse   : Point.t -> int -> int -> unit
     val draw_circle    : Point.t -> int -> unit
     val draw_arc       : Point.t -> int -> int -> int -> int 
-                       -> unit
+		      -> unit
     val fill_poly      : Point.t array -> unit
     val fill_ellipse   : Point.t -> int -> int -> unit
     val fill_circle    : Point.t -> int -> unit
@@ -16,13 +14,7 @@ module type T = sig
     val lineto         : Point.t -> unit
     val draw_image     : Graphics.image -> Point.t -> unit
     val get_image      : DoublePoint.t -> Graphics.image
-  end
-
-module SIZE = struct
-    module type T = sig
-	val width  : int
-	val height : int
-      end
+  
   end
 
 module SHIFT = struct

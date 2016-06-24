8e6821e26566cfb293d54bebb5189b5644ba2206
Miray Mann <miray.mann@qconnr.com>
2016-06-25 01:50:21 +0300

    introduce Runtime.Make

--- src/core/runtime.ml
+++ src/core/runtime.ml
@@ -1,31 +1,31 @@
-module Mode = struct
-    type t = | Run
-	     | Find of DotsOfDice.t
-	     | Return of DotsOfDice.t
+module Make (Breadcrumbs : BREADCRUMBS.T)
+                (Program : PROGRAM.T) = struct
+
+    type t = {    mode : RuntimeMode.t;
+	       program : Program.t;
+                crumbs : Breadcrumbs.t
+	     }
+
+    let make ~breadcrumbs:b 
+                 ~program:p = {    mode = RuntimeMode.Run;
+				program = p;
+                                 crumbs = b
+			      }
+
+    let with_mode m o = { o with mode = m }
+    let mode_of o = o.mode 
+
+    let active_command o =
+      let i = Breadcrumbs.last_place o.crumbs in
+      Program.get i o.program
+
+    let tick o =
+      let move = 
+	let open RuntimeMode in
+	match o.mode with
+        | Run 
+	| Find _   -> Breadcrumbs.increment
+	| Return _ -> Breadcrumbs.decrement
+      in
+      { o with crumbs = move o.crumbs }
   end
-
-type t = {    mode : Mode.t;
-           program : Program.t;
-            crumbs : Breadcrumbs.t
-	 }
-
-let make ~breadcrumbs:b 
-             ~program:p = {    mode = Mode.Run;
-                            program = p;
-                             crumbs = b
-			  }
-
-let with_mode m o = { o with mode = m }
-let mode_of o = o.mode 
-
-let active_command o =
-  let i = Breadcrumbs.last_place o.crumbs in
-  Program.get i o.program
-
-let tick o =
-  let move = match o.mode with
-             | Mode.Run 
-	     | Mode.Find _   -> Breadcrumbs.increment
-	     | Mode.Return _ -> Breadcrumbs.decrement
-  in
-  { o with crumbs = move o.crumbs }

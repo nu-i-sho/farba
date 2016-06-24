989e4badbf59056de3d927bf822da4f14c7d6917
Miray Mann <miray.mann@onnr.co>
2016-06-25 01:50:21 +0300

    introduce Runtime.Make

--- src/core/runtime.mli
+++ src/core/runtime.mli
@@ -1,16 +1,9 @@
-type t
+module Make (Breadcrumbs : BREADCRUMBS.T)
+                (Program : PROGRAM.T) 
+                         : sig
+    include RUNTIME.T
 
-module Mode : sig
-    type t = | Run
-	     | Find of DotsOfDice.t
-	     | Return of DotsOfDice.t
+    val make : breadcrumbs : Breadcrumbs.t 
+            ->     program : Program.t 
+            -> t
   end
-
-val make : breadcrumbs : Breadcrumbs.t 
-        ->     program : Program.t 
-        -> t
-
-val with_mode      : Mode.t -> t -> t
-val mode_of        : t -> Mode.t
-val tick           : t -> t
-val active_command : t -> Command.t

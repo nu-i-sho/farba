39979876ad66781571a052963620023f1913478c
Miray Mann <miray.mann@onnr.co>
2016-06-25 03:39:50 +0300

    move increment_... functions from RUNTIME_COUNTER to signature in Core as inc_..., rename RuntimeCounter.Make to -//-.Extend, Donor to Runtime and donor to base

--- src/shared/RUNTIME_COUNTER.ml
+++ src/shared/RUNTIME_COUNTER.ml
@@ -1,14 +1,9 @@
 module type T = sig
     type t
 
-    val zero    : t
     val summary : t -> int    
     val runs    : t -> int
     val finds   : t -> int
     val returns : t -> int
 
-    val increment_runs    : t -> t
-    val increment_finds   : t -> t
-    val increment_returns : t -> t
-	   
   end

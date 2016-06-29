b8b4ee2c0d44ba68e7496209a4a024404092fb1e
Miray Mann <miray.mann@qconnr.com>
2016-06-29 14:30:35 +0300

    remove module D from Nucleus

--- src/core/nucleus.ml
+++ src/core/nucleus.ml
@@ -1,20 +1,20 @@
 type t = Data.Nucleus.t
 
 open Data.Nucleus
-module D = Data
 
 let turn hand o =
   { o with gaze = Side.turn hand o.gaze }
 
 let inject cytoplasm o =
   { o with pigment = if cytoplasm = o.pigment then
-		     D.Pigment.None else
+		     Data.Pigment.None else
 		     o.pigment
   }
 
 let replicate relation cytoplasm o =
   {    gaze = Side.opposite o.gaze;
-    pigment = match relation with
-	      | D.Relation.Inverse -> Pigment.opposite o.pigment
-	      | D.Relation.Direct  -> o.pigment 
+    pigment = let open Data.Relation in
+              match relation with
+	      | Inverse -> Pigment.opposite o.pigment
+	      | Direct  -> o.pigment 
   } |> inject cytoplasm

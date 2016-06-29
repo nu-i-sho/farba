76f47eee5741f36eb292cf625363a5a9d4da24d8
Miray Mann <miray.mann@qconnr.com>
2016-06-29 14:28:50 +0300

    add inject to Nucleus

--- src/core/nucleus.ml
+++ src/core/nucleus.ml
@@ -6,16 +6,15 @@ module D = Data
 let turn hand o =
   { o with gaze = Side.turn hand o.gaze }
 
+let inject cytoplasm o =
+  { o with pigment = if cytoplasm = o.pigment then
+		     D.Pigment.None else
+		     o.pigment
+  }
+
 let replicate relation cytoplasm o =
   {    gaze = Side.opposite o.gaze;
-    pigment = 
-      let pigment = 
-	match relation with
-	| D.Relation.Inverse -> Pigment.opposite o.pigment
-	| D.Relation.Direct  -> o.pigment 
-      in
-  
-      if cytoplasm = pigment then
-	D.Pigment.None else
-	pigment
-  }
+    pigment = match relation with
+	      | D.Relation.Inverse -> Pigment.opposite o.pigment
+	      | D.Relation.Direct  -> o.pigment 
+  } |> inject cytoplasm

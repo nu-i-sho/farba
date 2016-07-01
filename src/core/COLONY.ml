041c996f695bb5683c5db5e2e9cc14fd9c9b0294
Miray Mann <miray.mann@onnr.co>
2016-07-01 17:08:03 +0300

    add iter, iterxy, fold, foldxy to COLONY

--- src/core/COLONY.ml
+++ src/core/COLONY.ml
@@ -4,4 +4,11 @@ module type T = sig
     val height : t -> int
     val width  : t -> int
     val get    : (int * int) -> t -> Pigment.t
+    val iter   : (Pigment.t -> unit) -> t -> unit
+    val iterxy : ((int * int) -> Pigment.t -> unit) -> t -> unit
+    val fold   : ('a -> Pigment.t -> 'a) -> 'a -> t -> 'a
+    val foldxy : ('a -> (int * int) -> Pigment.t -> 'a)
+              -> 'a
+              -> t
+              -> 'a
   end

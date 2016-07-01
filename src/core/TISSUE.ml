52569c395f2eb1d03185ab02affe5f01af0bab97
Miray Mann <miray.mann@onnr.co>
2016-07-01 17:16:24 +0300

    add empty to TISSUE

--- src/core/TISSUE.ml
+++ src/core/TISSUE.ml
@@ -1,6 +1,7 @@
 module type T = sig
     type t
 
-    val get : (int * int) -> t -> (Nucleus.t option)
-    val set : (int * int) -> (Nucleus.t option) -> t -> t
+    val empty : t
+    val get   : (int * int) -> t -> (Nucleus.t option)
+    val set   : (int * int) -> (Nucleus.t option) -> t -> t
   end

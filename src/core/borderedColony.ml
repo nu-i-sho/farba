f92510d801ccd4b6ad7207b35fa3e2d135bcf7be
Miray Mann <miray.mann@qconnr.com>
2016-07-01 17:15:22 +0300

    implement iter, iterxy, fold, foldxy in OrderedColony

--- src/core/borderedColony.ml
+++ src/core/borderedColony.ml
@@ -1,15 +1,43 @@
-module Decorate (Base : COLONY.T) = struct
-    type t = Base.t
-	   
-    let height o = 2 + (Base.height o)
-    let width  o = 2 + (Base.width  o)
+module Decorate (Colony : COLONY.T) = struct
     
+    type t = Colony.t
+
+    let height o = 2 + (Colony.height o)
+    let width  o = 2 + (Colony.width  o)
+
     let get (x, y) o = 
       if y = 0 || y = (height o) - 1 || 
          x = 0 || x = (width  o) - 1 then 
 	Data.Pigment.None else
-	Base.get ((x + 1), (y + 1)) o
+	Colony.get ((x + 1), (y + 1)) o
  
+    let iter f o =
+      for x = 0 to (width o) do
+	for y = 0 to (height o) do
+	  f (get (x, y) o)
+	done
+      done
+
+    let iterxy f o =
+      for x = 0 to (width o) do
+	for y = 0 to (height o) do
+	  f (x, y) (get (x, y) o)
+	done
+      done
+
+    let foldxy f acc o =
+      let h = height o and w = width o in
+      let rec fold ((x, y) as i) acc =
+	if y = h then acc else
+	  if x = w then fold (0, (y + 1)) acc else
+	    fold ((x + 1), y) (f acc i (get i o))
+      in
+      fold (0, 0) acc
+
+    let fold f =
+      let f acc _ = f acc in
+      foldxy f
+
     let decorate base = 
       base
   end

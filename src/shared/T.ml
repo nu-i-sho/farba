442de6deb31c43ebe0ae1d7be11e340d8d4d686d
Miray Mann <miray.mann@qconnr.com>
2016-07-01 17:19:38 +0300

    add indexes as paramters of ANATOMY_OBSERVER

--- src/shared/T.ml
+++ src/shared/T.ml
@@ -1,9 +1,11 @@
 module type ANATOMY_OBSERVER = sig
     type t
 
-    val set   : Data.Pigment.t -> t -> t
-    val reset : previous: Data.AnatomyItem.t
-             ->  current: Data.AnatomyItem.t
-             -> t
-             -> t
+    val set     : (int * int) -> Data.Pigment.t -> t -> t
+    val set_out : (int * int) -> Data.Nucleus.t -> t -> t
+    val reset   : (int * int)
+               -> previous: Data.AnatomyItem.t
+               ->  current: Data.AnatomyItem.t
+               -> t
+               -> t
   end

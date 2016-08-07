d6574fdfd9b368edb045cc2126ad42d6ae9aa163
Miray Mann <miray.mann@onnr.co>
2016-08-07 03:58:35 +0300

    extract level loading to Loader

--- src/core/level.mli
+++ src/core/level.mli
@@ -1,13 +1,13 @@
-module Load (Root : LEVELS_SOURCE_TREE.ROOT.T) : sig
-    type t 
+type t 
 
-    val active : t -> int * int
-    val height : t -> int
-    val width  : t -> int
-    val flora  : t -> Pigment.t Index.Map.t
-    val fauna  : t -> Nucleus.t Index.Map.t
-    val load   : DotsOfDice.t 
-              -> DotsOfDice.t 
-              -> DotsOfDice.t 
-              -> t
+val active : t -> int * int
+val height : t -> int
+val width  : t -> int
+val flora  : t -> Pigment.t Index.Map.t
+val fauna  : t -> Nucleus.t Index.Map.t
+
+module Loader : sig
+    module Make (Root : LEVELS_SOURCE_TREE.ROOT.T) : sig
+        val load : DotsOfDice.t -> DotsOfDice.t -> DotsOfDice.t -> t
+      end
   end

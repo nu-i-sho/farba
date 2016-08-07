6201e655dc3a4710fe0e311d011c7b435c84fbd7
Miray Mann <miray.mann@onnr.co>
2016-08-07 05:16:07 +0300

    introduce Matrix.optional

--- src/core/matrix.mli
+++ src/core/matrix.mli
@@ -3,6 +3,7 @@ type 'a t
 val empty    : int -> int -> 'a -> 'a t
 val of_array : 'a array array -> 'a t
 val of_map   : int -> int -> 'a -> 'a Index.Map.t -> 'a t
+val optional : int -> int -> 'a Index.Map.t -> 'a option t
 val height   : 'a t -> int
 val width    : 'a t -> int
 val get      : (int * int) -> 'a t -> 'a

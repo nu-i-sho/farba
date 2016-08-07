71c6128fbdcb808d5d0cec2ea6778ac7e0eadd50
Miray Mann <miray.mann@qconnr.com>
2016-08-07 22:02:54 +0300

    remove ActiveCell

--- src/core/activeCell.mli
+++
@@ -1,8 +0,0 @@
-module Make (Anatomy : ANATOMY.T) : sig
-    
-    include ACTIVE_CELL.T with type anatomy_t := Anatomy.t
-    val make : anatomy : Anatomy.t
-	    ->   index : int * int
-            -> nucleus : Nucleus.t
-	    -> out_t
-  end

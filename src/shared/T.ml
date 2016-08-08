b5c4ca642587feeb3d42f9316c88aa37510e7580
Miray Mann <miray.mann@qconnr.com>
2016-08-08 04:13:38 +0300

    style fix in T

--- src/shared/T.ml
+++ src/shared/T.ml
@@ -1,12 +1,12 @@
 module type TISSUE_OBSERVER = sig
     type t
 
-    val init    : int -> int -> t -> t
-    val set     : (int * int) -> Data.TissueItem.t -> t -> t
-    val reset   : (int * int)
-               -> Data.TissueItem.t
-               -> Data.TissueItem.t
-               -> t
-               -> t
+    val init  : int -> int -> t -> t
+    val set   : (int * int) -> Data.TissueItem.t -> t -> t
+    val reset : (int * int)
+             -> Data.TissueItem.t
+             -> Data.TissueItem.t
+             -> t
+             -> t
   end
 

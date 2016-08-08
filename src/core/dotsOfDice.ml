7a7a6fe5c58e1a16782ae1620bede2bf66ed12fd
Miray Mann <miray.mann@onnr.co>
2016-08-08 16:49:53 +0300

    introduce DotsOfDice.Map

--- src/core/dotsOfDice.ml
+++ src/core/dotsOfDice.ml
@@ -1,6 +1,20 @@
 type t = Data.DotsOfDice.t
 open Data.DotsOfDice
 
+module Map = Map.Make (
+                 struct
+                     type t = Data.DotsOfDice.t
+                     let to_int = 
+                       function | OOOOOO -> 6
+                                | OOOOO -> 5
+	                        | OOOO -> 4
+	                        | OOO -> 3
+	                        | OO -> 2
+	                        | O -> 1
+
+                     let compare x y = 
+                       compare (to_int x) (to_int y)
+                   end)
 let increment =
   function | OOOOOO -> O
            | OOOOO -> OOOOOO

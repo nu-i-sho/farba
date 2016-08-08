373ebe0719e3e385133264b53d484f3f197ebee1
Miray Mann <miray.mann@qconnr.com>
2016-08-08 07:16:31 +0300

    introduce Command

--- src/shared/data.ml
+++ src/shared/data.ml
@@ -82,3 +82,13 @@ module Hand = struct
              | Right
   end
 
+module Command = struct
+    type t = | Pass
+             | Move
+             | Turn of Hand.t
+             | Replicate of Relation.t
+             | Call of DotsOfDice.t
+             | Declare of DotsOfDice.t
+             | Nope
+             | End
+  end

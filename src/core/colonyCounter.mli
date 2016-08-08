e7bfd3bb7b9834f9d1ce5cb42cc83154ad72c3c5
Miray Mann <miray.mann@onnr.co>
2016-08-08 06:20:42 +0300

    remove unnessesary files

--- src/core/colonyCounter.mli
+++
@@ -1,8 +0,0 @@
-module Make (Colony : COLONY.T) : sig
-    type t
-	   
-    val calculate : Colony.t -> t
-    val count_of  : Pigment.t -> t -> int
-    val count     : t -> int
-  
-  end
--- src/core/observableAnatomy.mli
+++
@@ -1,9 +0,0 @@
-module Merge (Colony : COLONY.T) 
-	     (Tissue : TISSUE.T)
-           (Observer : T.ANATOMY_OBSERVER) : sig
-
-    include ANATOMY.T
-    val make :   colony : Colony.t
-	    -> observer : Observer.t
-	    -> t
-end

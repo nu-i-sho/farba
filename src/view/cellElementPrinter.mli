5e23720053f6d192b94bf4214f7e4c8f562ae1f8
Miray Mann <miray.mann@onnr.co>
2016-08-18 01:44:46 +0300

    introduce CellElementPrinter

---
+++ src/view/cellElementPrinter.mli
@@ -0,0 +1,20 @@
+type t
+
+val make : Canvas.t -> Scale.t -> ColorScheme.t -> t
+
+val set_index             : (int * int) -> t -> t
+val set_color_for_pigment : Data.Pigment.t -> t -> t
+val set_color_for_cancer  : t -> t
+val set_color_for_clot    : t -> t
+val set_color_for_virus   : t -> t
+val set_color_for_line    : t -> t
+
+val draw_hexagon : t -> t
+val fill_hexagon : t -> t
+val draw_nucleus : t -> t
+val fill_nucleus : t -> t
+
+val draw_open_eyes    : Data.Side.t -> t -> t
+val fill_open_eyes    : Data.Side.t -> t -> t
+val draw_engry_eyes   : Data.Side.t -> t -> t
+val draw_clotted_eyes : Data.Side.t -> t -> t

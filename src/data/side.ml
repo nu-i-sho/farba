9a9ae6e855b2fb8794e1dab4d0bc36fc3967998d
Miray Mann <miray.mann@onnr.co>
2016-08-19 01:53:34 +0300

    remove all functions from Data.Side

--- src/data/side.ml
+++ src/data/side.ml
@@ -4,29 +4,3 @@ type t = | Up
          | Down
          | LeftDown
          | RightDown
-
-let turn = 
-  function | Hand.Left  -> 
-	      ( function | Up        -> LeftUp
-	                 | LeftUp    -> LeftDown
-			 | LeftDown  -> Down
-			 | Down      -> RightDown
-			 | RightDown -> RightUp
-			 | RightUp   -> Up 
-	      )
-           | Hand.Right -> 
-	      ( function | Up        -> RightUp
-                         | RightUp   -> RightDown
-			 | RightDown -> Down
-			 | Down      -> LeftDown
-			 | LeftDown  -> LeftUp
-			 | LeftUp    -> Up
-	      )
-
-let opposite = 
-  function | Up        -> Down
-           | LeftUp    -> RightDown
-           | RightUp   -> LeftDown
-           | Down      -> Up
-           | LeftDown  -> RightUp
-           | RightDown -> LeftUp

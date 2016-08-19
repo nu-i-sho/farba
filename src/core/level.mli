6edc0dd8868adeb2e9750338c28c33f1a8e1cff0
Miray Mann <miray.mann@onnr.co>
2016-08-19 18:11:31 +0300

    move some contracts from Core to CONTRACTS

--- src/core/level.mli
+++ src/core/level.mli
@@ -8,7 +8,7 @@ val fauna  : t -> Data.Nucleus.t Index.Map.t
 val path   : t -> LevelPath.t
   
 module Loader : sig
-    module Make (LevelsSourceTree : LEVELS_SOURCE_TREE.ROOT.T) : sig
+    module Make (Tree : CONTRACTS.LEVELS_SOURCE_TREE.ROOT.T) : sig
         val load : LevelPath.t -> t
       end
   end

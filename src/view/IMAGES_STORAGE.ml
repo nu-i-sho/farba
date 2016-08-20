5cbbb9a3688f4033107b04f61fda4cd4ee77fd54
Miray Mann <miray.mann@onnr.co>
2016-08-20 03:43:59 +0300

    introduce IMAGES_STORAGE

---
+++ src/view/IMAGES_STORAGE.ml
@@ -0,0 +1,19 @@
+module type T = sig
+    type t
+    type result_t
+
+    module Command : sig
+        val get : Data.Command.t -> t -> result_t
+      end
+
+    module CallStackPoint : sig
+        val get : Data.DotsOfDice.t
+               -> Data.RuntimeMode.t
+               -> t
+               -> result_t
+      end
+         
+    val make : CommandColorScheme.t
+            -> CallStackPointColorScheme.t
+            -> t
+  end

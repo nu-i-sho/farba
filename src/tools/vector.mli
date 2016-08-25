fd93352889fc50d274c86e73b552468cdc76d4b2
Miray Mann <miray.mann@onnr.co>
2016-08-25 08:09:14 +0300

    add try_get to Vector

--- src/tools/vector.mli
+++ src/tools/vector.mli
@@ -1,6 +1,6 @@
 type 'a t
    
-val make   : (int -> 'a) -> int -> 'a t
-val length : 'a t -> int
-val get    : int -> 'a t -> 'a
-
+val make    : (int -> 'a) -> int -> 'a t
+val length  : 'a t -> int
+val get     : int -> 'a t -> 'a
+val try_get : int -> 'a t -> 'a option

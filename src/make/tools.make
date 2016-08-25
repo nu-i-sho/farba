fafc7a563694504787edf9efc0c2fca0e3cb26d9
Miray Mann <miray.mann@qconnr.com>
2016-08-26 01:25:57 +0300

    reimplement Data.Initable to Tools.Initable

--- src/make/tools.make
+++ src/make/tools.make
@@ -10,12 +10,14 @@ ocamlopt -for-pack Tools -c intPointMap.mli
 ocamlopt -for-pack Tools -c intPointMap.ml
 ocamlopt -for-pack Tools -c matrix.mli
 ocamlopt -for-pack Tools -c matrix.ml
+ocamlopt -for-pack Tools -c initable.mli
+ocamlopt -I ../../bin -for-pack Tools -c initable.ml
 ocamlopt -I ../../bin -for-pack Tools -c dotsOfDiceNodeMap.mli
 ocamlopt -I ../../bin -for-pack Tools -c dotsOfDiceNodeMap.ml
 
 ocamlopt -pack -o tools.cmx \
 vector.cmx intMap.cmx intPointMap.cmx matrix.cmx \
-dotsOfDiceNodeMap.cmx
+dotsOfDiceNodeMap.cmx initable.cmx
 
 mv tools.cmx ../../bin/tools.cmx
 mv tools.cmi ../../bin/tools.cmi
--- src/view/tissue.ml
+++ src/view/tissue.ml
@@ -1,26 +1,20 @@
-type t = | Maked of TissueFrame.t
-         | Inited of CellElementPrinter.t
-
 open Data
+open Tools
 open Nucleus
 open Cell
-   
+
+type t = (TissueFrame.t, CellElementPrinter.t) Initable.t
+       
 module P = CellElementPrinter
          
-let make frame =
-  Maked frame
+let make = Initable.make
 
 let init height width o =
-  match o with
-  | Inited _
-    -> failwith Fail.cannot_reinit_state
-  | Maked frame
-    -> TissueFrame.(
-         let scale =
-           Scale.make frame.height frame.width height width in
-         let printer = P.make frame.canvas scale frame.colors in 
-         Inited printer
-       )
+  let frame = Initable.made o in
+  Initable.init TissueFrame.(
+    let scale = Scale.make frame.height frame.width height width in
+    P.make frame.canvas scale frame.colors
+  )
      
 let nope printer = printer
 let print_cytoplasm pigment printer =
@@ -56,18 +50,16 @@ let print_cell is_active x printer =
           |> print_nucleus is_active (Cell.is_cancer x) x.nucleus
           
 let init_item i init o =
-  match o with
-  | Maked _
-    -> failwith Fail.state_is_not_inited
-  | Inited printer
-    -> let open TissueItemInit in
-       Inited (printer |> P.set_index i
-                       |> ( match init with
-                            | Out         -> nope
-                            | Cytoplasm x -> print_cytoplasm x
-                            | Static cell -> print_cell false cell
-                            | Active cell -> print_cell true  cell
-                          ))
+  TissueItemInit.(
+    o |> Initable.inited
+      |> P.set_index i
+      |> ( match init with
+           | Out         -> nope
+           | Cytoplasm x -> print_cytoplasm x
+           | Static cell -> print_cell false cell
+           | Active cell -> print_cell true  cell )
+      |> Initable.init
+  )
 
 let draw_nucleus_fully cell printer =
   printer |> P.draw_nucleus
@@ -76,59 +68,54 @@ let draw_nucleus_fully cell printer =
                  P.draw_open_eyes) cell.nucleus.gaze
          
 let update_item i update o =
-  match o with
-  | Maked _
-    -> failwith Fail.state_is_not_inited
-  | Inited printer
-    -> Inited
-         ( let open TissueItemUpdate in
-           match update with
-           | Inject (_, current)
-             -> printer |> P.set_index i
-                        |> print_nucleus (Cell.is_cancer current)
-                                          true current.nucleus 
-           | Extract (_, current)
-             -> printer |> P.set_index i
-                        |> fill_nucleus current
+  let printer = Initable.inited o in
+  Initable.init (
+    let open TissueItemUpdate in
+    match update with
+    | Inject (_, current)
+      -> printer |> P.set_index i
+                 |> print_nucleus (Cell.is_cancer current)
+                                   true current.nucleus 
+    | Extract (_, current)
+      -> printer |> P.set_index i
+                 |> fill_nucleus current
            
-           | Turn (previous, current)
-             -> let prev, curr = previous.nucleus,
-                                 current.nucleus in
-                if Cell.is_cancer current then
-                  printer |> P.set_index i
-                          |> P.set_pigment_color curr.pigment
-                          |> P.draw_engry_eyes prev.gaze
-                          |> P.set_virus_color
-                          |> P.draw_engry_eyes curr.gaze else
-                  printer |> P.set_index i
-                          |> fill_nucleus_eyes curr.pigment
-                                               prev.gaze
-                          |> fill_nucleus_eyes
-                               (Pigment.opposite curr.pigment)
-                                curr.gaze
-                          |> P.set_virus_color
-                          |> P.draw_open_eyes curr.gaze
+    | Turn (previous, current)
+      -> let prev, curr = previous.nucleus, current.nucleus in
+         if Cell.is_cancer current then
+           printer |> P.set_index i
+                   |> P.set_pigment_color curr.pigment
+                   |> P.draw_engry_eyes prev.gaze
+                   |> P.set_virus_color
+                   |> P.draw_engry_eyes curr.gaze else
+           printer |> P.set_index i
+                   |> fill_nucleus_eyes curr.pigment prev.gaze
+                   |> fill_nucleus_eyes
+                        (Pigment.opposite curr.pigment)
+                         curr.gaze
+                   |> P.set_virus_color
+                   |> P.draw_open_eyes curr.gaze
 
-           | VirusOut (_, current)
-             -> printer |> P.set_index i
-                        |> P.set_line_color
-                        |> draw_nucleus_fully current
+    | VirusOut (_, current)
+      -> printer |> P.set_index i
+                 |> P.set_line_color
+                 |> draw_nucleus_fully current
            
-           | Infect (_, current)
-             -> printer |> P.set_index i
-                        |> P.set_virus_color
-                        |> draw_nucleus_fully current
+    | Infect (_, current)
+      -> printer |> P.set_index i
+                 |> P.set_virus_color
+                 |> draw_nucleus_fully current
            
-           | DoClot (_, current)
-             -> printer |> P.set_index i
-                        |> P.set_clot_color
-                        |> P.fill_hexagon
-                        |> P.set_line_color
-                        |> P.draw_hexagon
-                        |> P.draw_clotted_eyes current
+    | DoClot (_, current)
+      -> printer |> P.set_index i
+                 |> P.set_clot_color
+                 |> P.fill_hexagon
+                 |> P.set_line_color
+                 |> P.draw_hexagon
+                 |> P.draw_clotted_eyes current
            
-           | MoveOut current
-             -> let is_cancer = Nucleus.is_cancer current in
-                printer |> P.set_index i
-                        |> print_nucleus true is_cancer current
-         )
+    | MoveOut current
+      -> let is_cancer = Nucleus.is_cancer current in
+         printer |> P.set_index i
+                 |> print_nucleus true is_cancer current
+  )

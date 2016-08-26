61983b07785edc4a8bbd7c72c27925f0e0e3de99
Miray Mann <miray.mann@qconnr.com>
2016-08-26 12:00:39 +0300

    introduce PROGRAM

--- src/engine/program.mli
+++ src/engine/program.mli
@@ -1,8 +1,2 @@
-type t
-   
-val make        : int -> Data.Command.t array -> Breadcrumbs.t -> t
-val width       : t -> int
-val lines_count : t -> int
-val crumbs      : t -> Breadcrumbs.t
-val with_crumbs : Breadcrumbs.t -> t -> t
-val line        : int -> t -> ProgramLine.t
+include PROGRAM.T
+val make : int -> Data.Command.t array -> Breadcrumbs.t -> t

ab4a0e1d2620beffc223471bff9c86b1d9718d37
Miray Mann <miray.mann@qconnr.com>
2016-08-28 04:54:33 +0300

    add resize_up and resize_down to ScrollableProgram

--- src/engine/program.mli
+++ src/engine/program.mli
@@ -1,2 +1,2 @@
-include PROGRAM.T
+include PROGRAM.T with type line_t = ProgramLine.t
 val make : int -> Data.Command.t array -> Breadcrumbs.t -> t
--- src/engine/scrollableProgram.mli
+++ src/engine/scrollableProgram.mli
@@ -1,9 +1,2 @@
-include PROGRAM.T
-      
+include SCROLLABLE_PROGRAM.T with type line_t = ProgramLine.t
 val make : int -> int -> Data.Command.t array -> Breadcrumbs.t -> t
-
-val view_lines_count : t -> int
-val view_line : int -> t -> ProgramLine.t
-val up : t -> t
-val down : t -> t
-val resize : int -> t -> t

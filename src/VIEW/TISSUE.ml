bb3b1d2cd1ad77d13d5f847d2d081fdeb7061da0
Miray Mann <miray.mann@onnr.co>
2016-09-01 17:22:46 +0300

    introduce VIEW

---
+++ src/VIEW/TISSUE.ml
@@ -0,0 +1,9 @@
+open Data
+
+module type T = sig
+    type t
+
+    val init        : int -> int -> t -> t
+    val init_item   : (int * int) -> TissueItemInit.t -> t -> t
+    val update_item : (int * int) -> TissueItemUpdate.t -> t -> t
+  end

5ff6ccf499c0e215b208964c3ce59968c8b4abb2
Miray Mann <miray.mann@qconnr.com>
2016-09-01 17:27:27 +0300

    change Core.Tissue signature with CORE.TISSUE including

--- src/core/tissue.mli
+++ src/core/tissue.mli
@@ -1,13 +1,6 @@
-type t
+include CORE.TISSUE.T
 
 val load        : Level.t -> t
-val height      : t -> int
-val width       : t -> int
-val init_items  : t -> Data.TissueItemInit.t Tools.Matrix.t
-val items       : t -> TissueItem.t Tools.Matrix.t
-val fauna       : t -> Data.Nucleus.t Tools.IntPointMap.t
-val flora       : t -> Data.Pigment.t Tools.IntPointMap.t
-val weaver      : t -> (int * int)
 val clot        : t -> ((int * int) * Data.Side.t) option
 val with_fauna  : Data.Nucleus.t Tools.IntPointMap.t -> t -> t
 val with_weaver : (int * int) -> t -> t

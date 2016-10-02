ff18537cf790dbbcde6aa121cc01aa4ea08f0787
Miray Mann <miray.mann@qconnr.com>
2016-10-02 19:52:53 +0300

    introduce Shared.DotsOfDiceNodeMap

---
+++ src/shared/dotsOfDiceNodeMap.ml
@@ -0,0 +1,19 @@
+open Data.Shared
+open SHARED
+
+module Make (ITEM : MODULE.T) = struct
+    module type ITEM = ITEM.T
+    module type NODE = DOTS_OF_DICE_NODE.MAKE (ITEM).T
+
+    type e = (module ITEM)
+    type t = (module NODE)
+    
+    let get dots (module Node : NODE) =
+      match dots with
+      | OOOOOO -> (module Node.OOOOOO : ITEM)
+      | OOOOO -> (module Node.OOOOO : ITEM)
+      | OOOO -> (module Node.OOOO : ITEM)
+      | OOO -> (module Node.OOO : ITEM)
+      | OO -> (module Node.OO : ITEM)
+      | O -> (module Node.O : ITEM)
+  end

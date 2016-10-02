9290afa2155a524fcb697bae0eea298cacf06ff0
Miray Mann <miray.mann@qconnr.com>
2016-10-02 16:23:11 +0300

    introduce Core.Side

---
+++ src/core/nucleus.ml
@@ -0,0 +1,21 @@
+open Data.Shared
+open Data.Tissue
+   
+type t = nucleus
+
+let turn hand o =
+  { o with gaze = Side.turn hand o.gaze
+  }
+
+let inject cytoplasm o =
+  { o with pigment = if cytoplasm = o.pigment then
+		       White else
+		       o.pigment
+  }
+
+let replicate relation o =
+  {    gaze = Side.opposite o.gaze;
+    pigment = match relation with
+	      | Inverse -> Proto.Pigment.opposite o.pigment
+	      | Direct  -> o.pigment 
+  }

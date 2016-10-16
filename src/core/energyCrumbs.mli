2f68e1cf5c092b258fc37357df7be2b848127806
Miray Mann <miray.mann@onnr.co>
2016-10-16 13:53:31 +0300

    reimplement Core.(Crumbs to EnergyCrumbs)

--- src/core/energyCrumbs.mli
+++ src/core/energyCrumbs.mli
@@ -5,7 +5,10 @@ val origin     : t
 val top        : t -> int * Crumb.t
 val item       : int -> t -> Crumb.t
 val maybe_item : int -> t -> Crumb.t option
-val succ       : t -> t
-val pred       : t -> t
-val split_top  : t -> t
 val exists     : int -> int -> t -> bool
+
+module Top : sig
+    val succ  : t -> t
+    val pred  : t -> t
+    val split : t -> t
+  end

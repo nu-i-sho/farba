a9120ffd75d8e2df407441982b183b0c35706af4
Miray Mann <miray.mann@onnr.co>
2016-11-10 02:51:54 +0200

    introduce Core.ArgsCrumbs

--- src/core/argsCrumbs.mli
+++ src/core/argsCrumbs.mli
@@ -1,21 +1,9 @@
-open Utils.Primitives
 open Data.Shared
 
-type e = | Active of args
-         | Static of args
-                   
-type active
+type e = args
 type t
 
-val active        : t -> (int * args) option
-val activate      : int -> t -> t
-val item          : int -> t -> e
-val maybe_item    : int -> t -> e option
-val of_string     : string -> t
-val update_active : (active -> active) -> t -> t
-  
-module Active : sig
-    val succ : active -> active
-    val pred : active -> active
-    val jump : int -> active -> active
-  end
+val of_string : string -> t
+val try_get   : int -> t -> e option
+val remove    : int -> t -> t
+val put       : int -> e -> t -> t

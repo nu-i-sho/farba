1f0d2e439eb94af1decc2207c3fb015465197160
Miray Mann <miraymann@localhost.localdomain>
2017-08-28 04:16:51 +0300

    introduce Die

---
+++ src/data/die.ml
@@ -0,0 +1,30 @@
+open Utils
+open Common
+
+type mode =
+  | Call
+  | Find
+  | Back
+  | Stay
+  
+type energy =
+  { generation : Dots.t;
+          mode : mode
+  }
+
+type arg =
+  { commnad : command;
+      param : Dots.t;
+       mode : mode
+  }
+
+type loop =
+  | Wait of Dots.t
+  | Work of {  origin : Dots.t;
+              current : Dots.t;
+            }
+
+type any =
+  | Energy of energy
+  | Arg of arg
+  | Loop of loop

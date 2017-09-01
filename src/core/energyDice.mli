21b20162e1e62e5b1cd48783d51fba3289a674c2
Miray Mann <miray.mann@onnr.co>
2017-09-02 02:26:04 +0300

    marge EnergyDots into EnergyDice

--- src/core/energyDice.mli
+++ src/core/energyDice.mli
@@ -1,2 +1,14 @@
-include ENERGY_DICE.T with type die = Data.Die.energy
-val with_top_mode : Data.Die.mode -> t -> t
+open Data
+
+type t
+
+val origin    : t
+val top_mode  : t -> Die.mode
+val top_index : t -> int
+val top_dies  : t -> Die.energy list
+val top_die   : t -> Die.energy
+val dies      : int -> t -> Die.energy list
+val die       : int -> t -> Die.energy
+val maybe_die : int -> t -> Die.energy option
+
+val with_top_mode : Die.mode -> t -> t

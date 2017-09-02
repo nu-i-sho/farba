7160f837eeb8c5c09a391cf8397dcf783f9c57d9
Miray Mann <miraymann@localhost.localdomain>
2017-09-02 06:24:26 +0300

    implement EnergyDice fully

--- src/utils/listOne.mli
+++ src/utils/listOne.mli
@@ -1,10 +1,11 @@
 type 'e t = 'e * ('e list)
-
+          
 val head    : 'e t -> 'e
 val tail    : 'e t -> 'e t
 val last    : 'e t -> 'e
 val to_list : 'e t -> 'e list
 val of_list : 'e list -> 'e t   
+val map     : ('a -> 'b) -> 'a t -> 'b t 
   
 val assoc     : 'k -> ('k * 'v) t -> 'v 
 val mem_assoc : 'k -> ('k * 'v) t -> bool

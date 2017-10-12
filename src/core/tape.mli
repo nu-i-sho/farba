d7f8877a3d41ee6eea9d737e22f8ada997c10763
Miray Mann <miraymann@localhost.localdomain>
2017-10-12 23:47:05 +0300

    raname Src to Tape

--- src/core/tape.mli
+++ src/core/tape.mli
@@ -1,10 +1,18 @@
 open Common
+open Utils
 
-type e = | Command of command
-         | OutOfRange of hand
 type t
 
-val make   : command list -> t
-val length : t -> int
-val get    : int -> t -> e
-val set    : int -> command -> t -> t
+val empty : t
+val length   : t -> int
+val commands : t -> command list
+val args     : t -> command Dots.Map.t IntMap.t
+val loops    : t -> Dots.t IntMap.t
+  
+val remove_item    : int -> t -> t
+val insert_command : command -> int -> t -> t
+val set_command    : command -> int -> t -> t
+val remove_loop    : int -> t -> t
+val set_loop       : Dots.t -> int -> t -> t
+val remove_arg     : Dots.t -> int -> t -> t
+val set_arg        : Dots.t -> command -> int -> t -> t

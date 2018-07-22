bbfe481675c6c31a4f26cd7e5dc161ab3a2e402e
Miray Mann <miray.mann@onnr.co>
2018-07-22 05:23:31 +0000

    tissue.ml edited online

--- src/core/tissue.ml
+++ src/core/tissue.ml
@@ -1,5 +1,7 @@
 open Common
 
+module CMap = Coord.Map
+
 type t = { height : int;
             width : int;
            nucleo : Nucleus.t Coord.Map.t;
@@ -16,10 +18,10 @@ let load src =
         match line.[i] with
         | ' ' -> cyto, nucleo
         | chr -> let p = ((j / 3), y)
-                 and cytoplasm = Pigment.of_char line.[i]
-                 and nucleus = Nucleus.of_chars line.[j] line.[k] in
-                 (Coord.Map.set p cytoplasm cyto),
-                 (Coord.Map.set p nucleus nucleo) in
+                 and c = Pigment.of_char  line.[i]
+                 and n = Nucleus.of_chars line.[j] line.[k] in
+                 (cyto   |> set p (Cytoplasm c))
+                 (nucleo |> set p (Nucleus c) in
       load_line cyto nucleo line y k l in
   
   let rec load cyto nucleo y = function
@@ -34,39 +36,46 @@ let load src =
                     cyto
                 } in
 
-  load Coord.Map.empty
-       Coord.Map.empty
+  load CMap.empty
+       CMap.empty
        0
        src
 
-let height o = o.height
-let width  o = o.width
-let clot   o = o.clot
-             
-let in_range (x, y) o =
-  x < (width  o) &&
-  y < (height o)
+let height_of o = o.height
+let width_of  o = o.width
+let clot_of   o = o.clot
   
-let out_of_range i o = 
-  not (in_range i o)
+let is_clotted o = 
+  match clot_f o with
+  | Some _ -> true
+  | None   -> false
   
-let cytoplasm i o = Coord.Map.find i o.cyto
-let nucleus   i o = Coord.Map.find i o.nucleo
+let is_in (x, y) o =
+  x < (width_of o) &&
+  y < (height_of o)
+  
+let is_out_of i o =
+  not (is_in i o)
 
-let maybe_cytoplasm i o = Coord.Map.find_opt i o.cyto
-let maybe_nucleus   i o = Coord.Map.find_opt i o.nucleo
-                  
-let set_nucleus i n o =
-  { o with
-    nucleo = Coord.Map.set i n o.nucleo
-  }
-  
-let remove_nucleus i o =
-  { o with
-    nucleo = Coord.Map.remove i o.nucleo
-  }
+let cytoplasm_at i o = CMap.find i o.cyto
+let nucleus_at   i o = CMap.find i o.nucleo
 
-let set_clot c o =
-  { o with
-    clot = Some c
-  }
+let maybe_cytoplasm_at i o = CMap.find_opt i o.cyto
+let maybe_nucleus_at   i o = CMap.find_opt i o.nucleo
+        
+let set item i o =
+  match item with
+  | Nucleus   x -> { o with nucleo = CMap.set i x o.nucleo }
+  | Cytoplasm x -> { o with cyto   = CMap.set i x o.cyto }
+  | Clot        -> { o with clot   = Some i }
+        
+let remove item i o =
+  match item with
+  | Nucleus   -> { o with nucleo = CMap.remove i o.nucleo }
+  | Cytoplasm -> { o with cyto   = CMap.remove i o.cyto }
+  | Clot      -> match clot_of o with
+                 | Some clot when (Coord.equels i clot) 
+                           -> { o with clot = None }
+                 | Some _  -> o
+                 | None    -> o
+

d4e8454311f4e8a8764240131623c8c80aad7666
Valentin Zakharenko <vvzakharenko@gmail.com>
2018-07-22 00:24:30 +0000

    pigment.ml edited online

--- src/core/pigment.ml
+++ src/core/pigment.ml
@@ -1,15 +1,15 @@
 type t = | White
          | Blue
          | Gray
-         | None
 
 let opposite =
-  function | Blue -> Gray
-           | Gray -> Blue
-           | o    -> o
+  function | White -> White
+           | Blue  -> Gray
+           | Gray  -> Blue
+           | o     -> o
            
 let of_char =
   function | 'g' -> Gray
            | 'b' -> Blue
            | 'w' -> White
-           | ___ -> None
+           | ___ -> raise (Invalid_arg "Pigment.of_char")

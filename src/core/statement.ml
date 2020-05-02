e6ea09e8d095253313fcd3b18f62af5b5719d68d
Valentyn <v.zakharenko@onnr.co>
2020-05-02 23:20:12 +0300

    introduce reach Statement

--- src/core/statement.ml
+++ src/core/statement.ml
@@ -1,15 +1,88 @@
-type t =
-  | Declare of declaration
-  | Do of { action : action;
-             count : Dots.t
-          }
+module Base = struct
+  module Declaration = struct
+    type 'parameter t =
+      | Procedure of Dots.t
+      | Parameter of 'parameter
+    end
 
- and declaration =
-  | Procedure of Dots.t
-  | Parameter of Dots.t
+  module Action = struct
+    type 'call t =
+      | Command of Command.t
+      | Call of 'call
+    end
+                     
+  type ('action, 'parameter) t =
+    | Declare of 'parameter Declaration.t
+    | Do of 'action
+  end
 
- and action =
-  | Command of Command.t
-  | Call of { procedure : Dots.t;
-              arguments : t Dots.Map.t
-            }
+module ForArgument = struct
+  type t = (Dots.t Base.Action.t, Dots.t) Base.t (*
+  type t = | Declare of | Procedure of Dots.t
+                        | Parameter of Dots.t
+           | Do of | Command of Command.t
+                   | Call of Dots.t   
+*)end
+
+module OfSourceCode = struct
+  module Call = struct
+    type t =
+      { procedure : Dots.t;
+        arguments : ForArgument.t Dots.Map.t
+      }
+    end
+
+  module Action = struct
+    type t =
+      { value : Call.t Base.Action.t;
+        count : Dots.t
+      }
+    end
+                
+  type t = (Action.t, Dots.t) Base.t (*
+  type t = | Declare of | Procedure of Dots.t
+                        | Parameter of Dots.t
+           | Do of { count : Dots.t;
+                     value : | Command of Command.t
+                             | Call of { procedure : Dots.t;
+                                         arguments : ForArgument.t Dots.Map.t 
+                                       }
+                   }
+*)end
+
+module InRuntime = struct
+  module Action = struct
+    module Loop = struct
+      type t =
+        { count : Dots.t;
+           iter : Dots.t
+        }
+      end
+    
+    type t =
+      { value : OfSourceCode.Call.t Base.Action.t;
+         loop : Loop.t
+      }
+    end
+
+  module Parameter = struct
+    type t =
+      {   name : Dots.t;
+        values : ForArgument.t List.t
+      }
+    end
+                
+  type t = (Action.t, Parameter.t) Base.t (*
+  type t = | Declare of | Procedure of Dots.t
+                        | Parameter of {   name : Dots.t
+                                         values : ForArgument.t List.t
+                                       }
+           | Do of {  loop : { count : Dots.t;
+                                iter : Dots.t
+                             };
+                     value : | Command of Command.t
+                             | Call of { procedure : Dots.t;
+                                         arguments : ForArgument.t Dots.Map.t 
+                                       }
+                    }
+*)end

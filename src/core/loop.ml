105f0522456201824eda05689063222fb97601e7
Valentyn <v.zakharenko@onnr.co>
2020-05-05 00:58:25 +0300

    introduce new Loop

--- src/core/loop.ml
+++ src/core/loop.ml
@@ -1,36 +1,17 @@
-open Utils
-
-module Die = struct
-    type t = | Work of Dots.t * Dots.t
-             | Wait of Dots.t
-
-    let make dots = Wait dots
-    let iter = function | Work (i, n) when n = i
-                                      -> Wait n
-                        | Wait     n  -> Work ((Dots.pred n), n) 
-                        | Work (i, n) -> Work ((Dots.pred i), n)
-                                       
-    let reset (Work (x, _) | Wait x) = Wait x
-  end
-
-module Dice = struct
-    type t = Die.t IntMap.t
-
-    let empty = IntMap.empty
-    let make  = IntMap.map Die.make 
-
-    let die      = IntMap.find
-    let die_opt  = IntMap.find_opt
-    let is_empty = IntMap.is_empty
-                
-    let set_die i n = IntMap.set i (Die.Wait n)
-    let remove_die  = IntMap.remove
+type t =
+  | Unactive of Dots.t
+  | Active of
+      { i : Dots.t;
+        n : Dots.t
+      }
       
-    let iter_die i o =
-      IntMap.set i (o |> die i
-                      |> Die.iter) o 
-      
-    let reset_die i o =
-      IntMap.set i (o |> die i
-                      |> Die.reset) o
-  end      
+let make n = Unactive n
+               
+let iter = function
+  | Unactive n -> Active { i = Dots.min; n }
+  | Active   o ->
+     if o.i = o.n then
+       Unactive o.n else
+       Active { o with
+                i = Dots.succ o.i
+              }
--- src/core/loop.mli
+++ src/core/loop.mli
@@ -1,21 +1,9 @@
-open Utils
+type t =
+  private | Unactive of Dots.t
+          | Active of
+              { i : Dots.t;
+                n : Dots.t
+              }
 
-module Die : sig
-    type t = private | Work of Dots.t * Dots.t
-                     | Wait of Dots.t
-  end
-
-module Dice : sig
-    type t
-
-    val empty : t
-    val make  : Dots.t IntMap.t -> t
-      
-    val die      : int -> t -> Die.t
-    val die_opt  : int -> t -> Die.t option
-    val is_empty : t -> bool
-      
-    val set_die    : int -> Dots.t -> t -> t
-    val remove_die : int -> t -> t
-    val iter_die   : int -> t -> t
-  end
+val make : Dots.t -> t
+val iter : t -> t

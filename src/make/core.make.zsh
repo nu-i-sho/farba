b8bee5c62bc7fa294948ab51dcf1a3ab3562dc46
Valentyn <v.zakharenko@onnr.co>
2020-07-19 09:58:02 +0300

    pack levels and improve core.make.zsh

--- src/make/core.make.zsh
+++ src/make/core.make.zsh
@@ -1,89 +1,112 @@
-module=Core
+#!/bin/zsh
 
-echo "$module build started"
-
-cd ../core
-
-bin="../../bin"
-package=${module:l}
-compiler="ocamlopt"
+caml=ocamlopt
 x="cmx"
 i="cmi"
 o="o"
-compile="$compiler -for-pack $module -c"
 
-$compile list.mli
-$compile list.ml 
-$compile map.mli
-$compile map.ml
-$compile dots.mli
-$compile dots.ml
-$compile hand.ml
-$compile gene.ml
-$compile nature.ml
-$compile pigment.mli
-$compile pigment.ml
-$compile side.mli
-$compile side.ml
-$compile nucleus.mli
-$compile nucleus.ml
-$compile command.ml
-$compile tissue.mli
-$compile tissue.ml
-$compile cursor.mli
-$compile cursor.ml
-$compile energy.mli
-$compile energy.ml
-$compile statement.ml
-$compile source.mli
-$compile source.ml
-$compile tape.mli
-$compile tape.ml
-$compile processor.mli
-$compile processor.ml
-$compile OBSERV.ml
-$compile subject.mli
-$compile subject.ml
+module=Core
+package=${module:l}
 
-$compile level_001.ml
-$compile level_002.ml
-$compile level_003.ml
-$compile level_004.ml
-$compile level_005.ml
-$compile level_006.ml
-$compile level_007.ml
-$compile level_008.ml
-$compile level_009.ml
-$compile level_009.ml
-$compile levels.mli
-$compile levels.ml
-$compile config.mli
-$compile config.ml
-$compile program.mli
-$compile program.ml
+echo "$module build started"
 
-$compiler -pack -o $package.$x \
-list.$x map.$x dots.$x hand.$x gene.$x nature.$x \
-pigment.$x side.$x nucleus.$x command.$x tissue.$x cursor.$x energy.$x \
-source.$x statement.$x tape.$x processor.$x OBSERV.$x subject.$x \
-level_001.$x level_002.$x level_003.$x level_004.$x \
-level_005.$x level_006.$x level_007.$x level_008.$x level_009.$x \
-levels.$x config.$x program.$x
+cd ../$package
+bin=../../bin
 
-mv $package.$x $bin/$package.$x
-mv $package.$i $bin/$package.$i
-mv $package.$o $bin/$package.$o
+mv_bins () {
+    # $1 - package name
+    # $2 - bin directory
 
-find . -type f -iname \*.$x -delete
-find . -type f -iname \*.$i -delete
-find . -type f -iname \*.$o -delete
+    mv $1.$x $2/$1.$x
+    mv $1.$i $2/$1.$i
+    mv $1.$o $2/$1.$o
+}
 
-$compiler -output-obj -o $package.$o unix.cmxa $bin/$package.$x
+rm_bins () {
+    # $1 - package name
+    # $2 - bin directory
 
-rm $bin/$package.$x
-rm $bin/$package.$i
-rm $bin/$package.$o
+    rm $2/$1.$x
+    rm $2/$1.$i
+    rm $2/$1.$o
+}
 
+rm_tmp_bins () {
+    find . -type f -iname \*.$x -delete
+    find . -type f -iname \*.$i -delete
+    find . -type f -iname \*.$o -delete
+}
+
+src_files=(
+    list          mli ml
+    map           mli ml
+    dots          mli ml
+    hand              ml
+    gene              ml
+    nature            ml
+    pigment       mli ml
+    side          mli ml
+    nucleus       mli ml
+    command           ml
+    tissue        mli ml
+    cursor        mli ml
+    energy        mli ml
+    statement         ml
+    source        mli ml
+    tape          mli ml
+    processor     mli ml
+    OBSERV            ml
+    subject       mli ml
+)
+
+bins=()
+file=null
+for f in $src_files
+do
+    if   [[ $f == "ml" || $f == "mli" ]]
+    then $caml -for-pack $module -c $file.$f
+    else file=$f
+	 bins+=$file.$x
+    fi
+done
+
+levels_module=Levels
+levels_package=${levels_module:l}
+
+echo "$levels_module build started"
+
+cd $levels_package
+prev_bin=$bin
+bin="../$prev_bin"
+
+levels_bins=()
+for level in *
+do
+    if   [[ ${level:e} == "ml" ]]
+    then $caml -I .. -for-pack $module.$levels_module -c $level
+	 levels_bins+=${leve:r}.$x
+    fi
+done
+
+$caml -pack -for-pack $module -o $levels_package.$x $level_bins[@]
+mv_bins $levels_package $bin
+rm_tmp_bins
+
+bin=$prev_bin
+bins+=$bin/$levels_package.$x
+cd ..
+
+echo "$levels_module build completed"
+
+$caml -pack -o $package.$x $bins[@]
+
+rm_bins $levels_package $bin
+mv_bins $package $bin
+rm_tmp_bins
+
+$caml -output-obj -o $package.$o unix.cmxa $bin/$package.$x
+
+rm_bins $package $bin
 mv $package.$o $bin/$package.$o
 
 echo "$module build completed"

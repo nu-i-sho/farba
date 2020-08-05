c45a8c881d25c0c0207fd016d95259ab9036bf23
Valentyn <v.zakharenko@onnr.co>
2020-08-05 20:41:34 +0300

    initialize polimorphic variants from OCaml as consts in C++

--- src/api/api.cpp
+++ src/api/api.cpp
@@ -6,6 +6,7 @@ extern "C" {
 #include "caml/alloc.h"
 }
 
+#include <map>
 #include "api.hpp"
 #include "data/domain.hpp"
 
@@ -14,11 +15,27 @@ using 𝙲𝚞𝚛𝚜𝚘𝚛 = 𝙰𝚙𝚒::𝙴𝚟𝚎𝚗𝚝𝚜𝙾𝚏:
 using ResultOf = 𝙰𝚙𝚒::𝙵𝚒𝚕𝚎::ResultOf;
 
 #define CAML_FUNC(F, NAME) \
-  static const value* F = NULL; \
-  if (F == NULL) F = caml_named_value(NAME)
+  static const value* F = nullptr; \
+  if (F == nullptr) F = caml_named_value(NAME)
 
-#define CAML_POLIMORPHIC_VARIANT(NAME) \
-  static const int Caml_##NAME = caml_hash_variant(#NAME);
+#define CAML_POLIMORPHIC_VARIANT_AS_CONST(NAME) \
+  static const int NAME = caml_hash_variant(#NAME);
+
+namespace Caml {
+  CAML_POLIMORPHIC_VARIANT_AS_CONST(Level_is_missing)
+  CAML_POLIMORPHIC_VARIANT_AS_CONST(Level_is_unavailable)
+  CAML_POLIMORPHIC_VARIANT_AS_CONST(Permission_denied)
+  CAML_POLIMORPHIC_VARIANT_AS_CONST(Backup_not_found)
+  CAML_POLIMORPHIC_VARIANT_AS_CONST(Backup_is_corrupted)
+  CAML_POLIMORPHIC_VARIANT_AS_CONST(Name_is_empty)
+  CAML_POLIMORPHIC_VARIANT_AS_CONST(File_already_exists)
+
+  CAML_POLIMORPHIC_VARIANT_AS_CONST(Success)
+  CAML_POLIMORPHIC_VARIANT_AS_CONST(Fail)
+  CAML_POLIMORPHIC_VARIANT_AS_CONST(Clotted)
+  CAML_POLIMORPHIC_VARIANT_AS_CONST(Rev_gaze)
+  CAML_POLIMORPHIC_VARIANT_AS_CONST(Self_clotted)
+}
 
 value Val_api_ptr(Api* api) {
   value caml_api = caml_alloc_small(1, Abstract_tag);
@@ -89,66 +106,47 @@ void Api::unsubscribe_caml_cursor_events() {
   _state = state;
 }
 
-CAML_POLIMORPHIC_VARIANT(Level_is_missing)
-CAML_POLIMORPHIC_VARIANT(Level_is_unavailable)
-CAML_POLIMORPHIC_VARIANT(Permission_denied)
-CAML_POLIMORPHIC_VARIANT(Backup_not_found)
-CAML_POLIMORPHIC_VARIANT(Backup_is_corrupted)
-CAML_POLIMORPHIC_VARIANT(Name_is_empty)
-CAML_POLIMORPHIC_VARIANT(File_already_exists)
-
 Api::File::File(Api* api) {
   _api = api;
-}
-
-ResultOf::OpenNew ResultOf_OpenNew_val(value v) {
-  switch (Int_val(v)) {
-  case 0:  return ResultOf::OpenNew::OK;
-  case 1: //Caml_Level_is_missing:
-           return ResultOf::OpenNew::Level_is_missing;
-  case 2: //Caml_Level_is_unavailable:
-           return ResultOf::OpenNew::Level_is_unavailable;
-  default: throw "IMPOSSIBLE CASE";
-  }
 }
 
 ResultOf::OpenNew Api::File::open_new(int level) {
   if (!_api -> is_empty())
     _api -> unsubscribe_caml_cursor_events();
+
+  static const map<int, ResultOf::OpenNew> results = {
+    { 0,                          ResultOf::OpenNew::OK                   },
+    { Caml::Level_is_missing,     ResultOf::OpenNew::Level_is_missing     },
+    { Caml::Level_is_unavailable, ResultOf::OpenNew::Level_is_unavailable }
+  };
   
   CAML_FUNC(caml_open_new, "File.open_new");
-  auto result = ResultOf_OpenNew_val(
+  auto result = results.at(Int_val(
     _api -> extract_and_save_state(
       caml_callback(
         *caml_open_new,
         Val_int(level)
       )
     )
-  );
+  ));
 
   _api -> subscribe_caml_cursor_events();
   return result;
 }
 
-ResultOf::Restore ResultOf_Restore_val(value v) {
-  switch (Int_val(v)) {
-  case 0:  return ResultOf::Restore::OK;
-  case 1: //Caml_Permission_denied:
-           return ResultOf::Restore::Permission_denied;
-  case 2: //Caml_Backup_not_found:
-           return ResultOf::Restore::Backup_not_found;
-  case 3: //Caml_Backup_is_corrupted:
-           return ResultOf::Restore::Backup_is_corrupted;
-  default: throw "IMPOSSIBLE CASE";
-  }
-}
-
 ResultOf::Restore Api::File::restore(int level, const char* name) {
   if (!_api -> is_empty())
     _api -> unsubscribe_caml_cursor_events();
+
+  static const map<int, ResultOf::Restore> results = {
+    { 0,                         ResultOf::Restore::OK                  },
+    { Caml::Permission_denied,   ResultOf::Restore::Permission_denied   },
+    { Caml::Backup_not_found,    ResultOf::Restore::Backup_not_found    },
+    { Caml::Backup_is_corrupted, ResultOf::Restore::Backup_is_corrupted }
+  };
   
   CAML_FUNC(caml_restore, "File.restore");
-  auto result = ResultOf_Restore_val(
+  auto result = results.at(Int_val(
     _api -> extract_and_save_state(
       caml_callback2(
         *caml_restore,
@@ -156,64 +154,51 @@ ResultOf::Restore Api::File::restore(int level, const char* name) {
         caml_copy_string(name)
       )
     )
-  );
+  ));
 
   _api -> subscribe_caml_cursor_events();
   return result;
 }
 
-ResultOf::Save ResultOf_Save_val(value v) {
-  switch (Int_val(v)) {
-  case 0:  return ResultOf::Save::OK;
-  case 1: //Caml_Permission_denied:
-           return ResultOf::Save::Permission_denied;
-  case 2: //Caml_Name_is_empty:
-           return ResultOf::Save::Name_is_empty;
-  default: throw "IMPOSSIBLE CASE";
-  }
-}
-
 ResultOf::Save Api::File::save() {
-  if (_api->is_empty())
-    return ResultOf::Save::Nothing_to_save;
-  
+  if (_api -> is_empty()) return  ResultOf::Save::Nothing_to_save;
+
+  static const map<int, ResultOf::Save> results = {
+    { 0,                          ResultOf::Save::OK                },
+    { Caml::Permission_denied,    ResultOf::Save::Permission_denied },
+    { Caml::Name_is_empty,        ResultOf::Save::Name_is_empty     }			   
+  };
+    
   _api -> unsubscribe_caml_cursor_events();
 
   CAML_FUNC(caml_save, "File.save");
-  auto result = ResultOf_Save_val(
+  auto result = results.at(Int_val(
     _api -> extract_and_save_state(
       caml_callback(
         *caml_save,
         _api -> _state
       )
     )
-  );
+  ));
 
   _api -> subscribe_caml_cursor_events();
   return result;
 }
 
-ResultOf::SaveAs ResultOf_SaveAs_val(value v) {
-  switch (Int_val(v)) {
-  case 0:  return ResultOf::SaveAs::OK;
-  case 1: //Caml_Permission_denied:
-           return ResultOf::SaveAs::Permission_denied;
-  case 2: //Caml_Name_is_empty:
-           return ResultOf::SaveAs::Name_is_empty;
-  case 3: //Caml_File_already_exists:
-           return ResultOf::SaveAs::File_already_exists;
-  default: throw "IMPOSSIBLE CASE";
-  }
-}
-    
 ResultOf::SaveAs Api::File::save_as(const char* name) {
-  if (_api -> is_empty())
-    return ResultOf::SaveAs::Nothing_to_save;
+  if (_api -> is_empty()) return ResultOf::SaveAs::Nothing_to_save;
 
+  static const map<int, ResultOf::SaveAs> results = {
+    { 0,                          ResultOf::SaveAs::OK                  },
+    { Caml::Permission_denied,    ResultOf::SaveAs::Permission_denied   },
+    { Caml::Name_is_empty,        ResultOf::SaveAs::Name_is_empty       },
+    { Caml::File_already_exists,  ResultOf::SaveAs::File_already_exists } 
+  };
+  
   _api -> unsubscribe_caml_cursor_events();
   
   CAML_FUNC(caml_save_as, "File.save_as");
-  auto result = ResultOf_SaveAs_val(
+  auto result = results.at(Int_val(
     _api -> extract_and_save_state(
       caml_callback2(
         *caml_save_as,
@@ -221,54 +206,10 @@ ResultOf::SaveAs Api::File::save_as(const char* name) {
         _api -> _state
       )
     )
-  );
+  ));
 
   _api -> subscribe_caml_cursor_events();
   return result;
-}
-
-CAML_POLIMORPHIC_VARIANT(Success)
-CAML_POLIMORPHIC_VARIANT(Fail)
-CAML_POLIMORPHIC_VARIANT(Clotted)
-CAML_POLIMORPHIC_VARIANT(Rev_gaze)
-CAML_POLIMORPHIC_VARIANT(Self_clotted)
-
-𝙲𝚞𝚛𝚜𝚘𝚛::MovedMindStatus MovedMindStatus_val(value v) {
-  switch (Int_val(v)) {
-  case 1:// Caml_Success:
-    return 𝙲𝚞𝚛𝚜𝚘𝚛::MovedMindStatus::Success;
-  case 2:// Caml_Fail:
-    return 𝙲𝚞𝚛𝚜𝚘𝚛::MovedMindStatus::Fail;
-  default: throw "IMPOSSIBLE CASE";
-  }
-}
-
-𝙲𝚞𝚛𝚜𝚘𝚛::MovedBodyStatus MovedBodyStatus_val(value v) {
-  switch (Int_val(v)) {
-  case 1: //Caml_Success:
-    return 𝙲𝚞𝚛𝚜𝚘𝚛::MovedBodyStatus::Success;
-  case 2: //Caml_Fail:
-    return 𝙲𝚞𝚛𝚜𝚘𝚛::MovedBodyStatus::Fail;
-  case 3: //Caml_Clotted:
-    return 𝙲𝚞𝚛𝚜𝚘𝚛::MovedBodyStatus::Clotted;
-  case 4: //Caml_Rev_gaze:
-    return 𝙲𝚞𝚛𝚜𝚘𝚛::MovedBodyStatus::Rev_gaze;
-  default: throw "IMPOSSIBLE CASE";
-  }
-}
-
-𝙲𝚞𝚛𝚜𝚘𝚛::ReplicatedStatus ReplicatedStatus_val(value v) {
-  switch (Int_val(v)) {
-  case 1: //Caml_Success:
-    return 𝙲𝚞𝚛𝚜𝚘𝚛::ReplicatedStatus::Success;
-  case 2: //Caml_Fail:
-    return 𝙲𝚞𝚛𝚜𝚘𝚛::ReplicatedStatus::Fail;
-  case 3: //Caml_Clotted:
-    return 𝙲𝚞𝚛𝚜𝚘𝚛::ReplicatedStatus::Clotted;
-  case 4: //Caml_Self_clotted:
-    return 𝙲𝚞𝚛𝚜𝚘𝚛::ReplicatedStatus::Self_clotted;
-  default: throw "IMPOSSIBLE CASE";
-  }
 }
 
 template <typename T>
@@ -322,6 +263,10 @@ Change<T> Change_val(T (*T_val)(value), value v) {
   };
 }
 
+Change<TissueCell> Change_of_TissueCell_val(value v) {
+  return Change_val<TissueCell>(Tissue_cell_val, v);
+}
+
 Change<tuple<TissueCell, TissueCell>> Change_of_TissueCell_tuple_val(value v) {
   return Change_val<tuple<TissueCell, TissueCell>>(
     [] (value v) { return Pair_val(Tissue_cell_val, v); },
@@ -330,28 +275,31 @@ Change<tuple<TissueCell, TissueCell>> Change_of_TissueCell_tuple_val(value v) {
 
 extern "C" {
   CAMLprim value on_cursor_turned(value caml_event, value caml_api) {
-    CAMLparam2(caml_event, caml_api);
-    Api* api = (Api*)Field(caml_api, 0);
-    
-    auto hand   = Enum_struct_val<Hand>(Field(caml_event, 0));
-    auto change = Change_val<TissueCell>(
-      Tissue_cell_val,
-      Field(caml_event, 1));
+    CAMLparam2(caml_event, caml_api);    
+
+    auto api    = (Api*)                   Field(caml_api,   0);
+    auto hand   = Enum_struct_val<Hand>(   Field(caml_event, 0));
+    auto change = Change_of_TissueCell_val(Field(caml_event, 1));
 
     auto event = 𝙲𝚞𝚛𝚜𝚘𝚛::Turned { hand, change };
     api -> events_of() -> cursor() -> _turned -> send(event);
 
     CAMLreturn(Val_api_ptr(api));
   }
-
+ 
   CAMLprim value on_cursor_moved_mind(value caml_event, value caml_api) {
     CAMLparam2(caml_event, caml_api);
-    Api* api = (Api*)Field(caml_api, 0);
 
-    auto direction = Enum_struct_val<Side>         (Field(caml_event, 0));
+    static const map<int, 𝙲𝚞𝚛𝚜𝚘𝚛::MovedMindStatus> statuses = {
+      { Caml::Success, 𝙲𝚞𝚛𝚜𝚘𝚛::MovedMindStatus::Success },
+      { Caml::Fail,    𝙲𝚞𝚛𝚜𝚘𝚛::MovedMindStatus::Fail    }
+    };
+
+    auto api       = (Api*)                         Field(caml_api,   0);
+    auto direction = Enum_struct_val<Side>(         Field(caml_event, 0));
     auto change    = Change_of_TissueCell_tuple_val(Field(caml_event, 1));
-    auto status    = MovedMindStatus_val           (Field(caml_event, 2));
-    
+    auto status    = statuses.at(Int_val(           Field(caml_event, 2)));
+
     auto event = 𝙲𝚞𝚛𝚜𝚘𝚛::MovedMind { direction, change, status };
     api -> events_of() -> cursor() -> _moved_mind -> send(event);
     
@@ -360,12 +308,19 @@ extern "C" {
 
   CAMLprim value on_cursor_moved_body(value caml_event, value caml_api) {
     CAMLparam2(caml_event, caml_api);
-    Api* api = (Api*)Field(caml_api, 0);
-
-    auto direction = Enum_struct_val<Side>         (Field(caml_event, 0));
-    auto change    = Change_of_TissueCell_tuple_val(Field(caml_event, 1));
-    auto status    = MovedBodyStatus_val           (Field(caml_event, 2));
     
+    static const map<int, 𝙲𝚞𝚛𝚜𝚘𝚛::MovedBodyStatus> statuses = {
+      { Caml::Success,  𝙲𝚞𝚛𝚜𝚘𝚛::MovedBodyStatus::Success  },
+      { Caml::Fail,     𝙲𝚞𝚛𝚜𝚘𝚛::MovedBodyStatus::Fail     },
+      { Caml::Clotted,  𝙲𝚞𝚛𝚜𝚘𝚛::MovedBodyStatus::Clotted  },
+      { Caml::Rev_gaze, 𝙲𝚞𝚛𝚜𝚘𝚛::MovedBodyStatus::Rev_gaze }
+    };
+
+    auto api       = (Api*)                         Field(caml_api,   0);
+    auto direction = Enum_struct_val<Side>(         Field(caml_event, 0));
+    auto change    = Change_of_TissueCell_tuple_val(Field(caml_event, 1));
+    auto status    = statuses.at(Int_val(           Field(caml_event, 2)));
+
     auto event = 𝙲𝚞𝚛𝚜𝚘𝚛::MovedBody { direction, change, status };
     api -> events_of() -> cursor() -> _moved_body -> send(event);
     
@@ -374,12 +329,19 @@ extern "C" {
 
   CAMLprim value on_cursor_replicated(value caml_event, value caml_api) {
     CAMLparam2(caml_event, caml_api);
-    Api* api = (Api*)Field(caml_api, 0);
 
-    auto gene      = Enum_struct_val<Gene>         (Field(caml_event, 0));
-    auto direction = Enum_struct_val<Side>         (Field(caml_event, 1));
+    static const map<int, 𝙲𝚞𝚛𝚜𝚘𝚛::ReplicatedStatus> statuses = {
+      { Caml::Success,      𝙲𝚞𝚛𝚜𝚘𝚛::ReplicatedStatus::Success      },
+      { Caml::Fail,         𝙲𝚞𝚛𝚜𝚘𝚛::ReplicatedStatus::Fail         },
+      { Caml::Clotted,      𝙲𝚞𝚛𝚜𝚘𝚛::ReplicatedStatus::Clotted      },
+      { Caml::Self_clotted, 𝙲𝚞𝚛𝚜𝚘𝚛::ReplicatedStatus::Self_clotted }
+    };
+    
+    auto api       = (Api*)                         Field(caml_api,   0);
+    auto gene      = Enum_struct_val<Gene>(         Field(caml_event, 0));
+    auto direction = Enum_struct_val<Side>(         Field(caml_event, 1));
     auto change    = Change_of_TissueCell_tuple_val(Field(caml_event, 2));
-    auto status    = ReplicatedStatus_val          (Field(caml_event, 3));
+    auto status    = statuses.at(Int_val(           Field(caml_event, 3)));
     
     auto event = 𝙲𝚞𝚛𝚜𝚘𝚛::Replicated { direction, change, status, gene };
     api -> events_of() -> cursor() -> _replicated -> send(event);

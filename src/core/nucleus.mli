44f450145d08a4789dcb1a36d2e0ccac705f8525
Valentin Zakharenko <v.zakharenko@onnr.co>
2021-12-24 01:16:11 +0200

    introduce turn_(left | right)

--- src/core/nucleus.mli
+++ src/core/nucleus.mli
@@ -5,6 +5,8 @@ type t = private
 
 val make       : Pigment.t -> Side.t -> t
 val is_bastard : t -> bool
+val turn_left  : t -> t
+val turn_right : t -> t
 val turn       : Hand.t -> t -> t
 val look_back  : t -> t
 val inject     : Pigment.t -> t -> t

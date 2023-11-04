b7ef4dc2d68a1c6877e0676412a7725fc72fa2eb
Valentyn Zakharenko <vvzakharenko@gmail.com>
2023-11-04 22:51:05 +0200

    style up Tick.succ casses middle/left/right

--- src/core/tape.ml
+++ src/core/tape.ml
@@ -20,39 +20,42 @@ module Make = functor (Num : SEQUENTIAL.T) -> struct
                 ) Statement.t
     
   type 'cmd t = 
-    | Middle of
-        { prev :  'cmd e list;
-          curr : ('cmd e) * cursor;
-          next :  'cmd e list
-        }
-    | LeftEnd of
-        { curr : cursor;
-          next : ('cmd e) * ('cmd e list);
-        }
-    | RightEnd of
-        { prev : ('cmd e) * ('cmd e list);
-          curr : cursor;
-        }
+    | Middle of 'cmd middle
+    | Left   of 'cmd left
+    | Right  of 'cmd right
     | Empty
+    
+   and 'cmd middle = 
+     { prev :  'cmd e list;
+       curr : ('cmd e) * cursor;
+       next :  'cmd e list
+     }
+   and 'cmd left =
+     { curr : cursor;
+       next : ('cmd e) * ('cmd e list);
+     }
+   and 'cmd right = 
+     { prev : ('cmd e) * ('cmd e list);
+       curr : cursor;
+     } 
 
   type error =
     | Nothing_to_do_with_empty_program
     | Procedure_not_found of Num.t     
-
+                           
   let load = function
-    | []      -> Empty
-    | h :: t  ->
-       LeftEnd
-         { curr = Move (Call Energy.origin);
-           next = let internalize =
-                    Statement.( function
-                      | Perform x -> Perform  x
-                      | Execute x -> Execute (x, [])
-                      | Declare x -> Declare (x, [])                           
-                    ) in
-                  (h |> internalize),
-                  (t |> List.map internalize)
-         }
+    | []     -> Empty
+    | h :: t ->
+       Left { curr = Move (Call Energy.origin);
+              next = let internalize =
+                       Statement.( function
+                         | Perform x -> Perform  x
+                         | Execute x -> Execute (x, [])
+                         | Declare x -> Declare (x, [])                           
+                       ) in
+                     (h |> internalize),
+                     (t |> List.map internalize)
+            }
                            
   module Tick = struct
     type nonrec 'cmd t =
@@ -66,188 +69,136 @@ module Make = functor (Num : SEQUENTIAL.T) -> struct
     let succ o =
       let open Direction in
       let open Statement in
-      let with_command  x tape = Ok { command = Some x; tape }
-      and without_command tape = Ok { command = None;   tape } in
+
+      let middle x = Ok { command = None; tape = Middle x }
+      and left   x = Ok { command = None; tape = Left   x }
+      and right  x = Ok { command = None; tape = Right  x } in
+
       match o.tape with
       | Empty -> Error Nothing_to_do_with_empty_program
       
-      | Middle
-        { prev = ps :: pss;
-          curr = cs, (Move (( Find (_, Back, _)
-                            | Return (_, Back)
-                            | Close _) as e));
-          next = nss
-        } -> without_command
-             (Middle { prev = pss;
-                       curr = ps, (Apply e);
-                       next = cs :: nss
-                     })
-      | Middle
-        { prev = [];
-          curr = cs, (Move (( Find (_, Back, _)
-                            | Return (_, Back)
-                            | Close _) as e));
-          next = nss
-        } -> without_command
-               (LeftEnd { curr = Apply e;
-                          next = cs, nss
-                        })
+      | Middle  { prev = ps :: pss;
+                  curr = cs, (Move (( Find (_, Back, _)
+                                    | Return (_, Back)
+                                    | Close _) as e));
+                  next = nss
+                } -> middle { prev = pss;
+                              curr = ps, (Apply e);
+                              next = cs :: nss
+                            }
+      | Middle  { prev = [];
+                  curr = cs, (Move (( Find (_, Back, _)
+                                    | Return (_, Back)
+                                    | Close _) as e));
+                  next = nss
+                } ->   left { curr = Apply e;
+                              next = cs, nss
+                            }
            
-      | Middle 
-        { prev = pss;
-          curr = cs, (Move e);
-          next = ns :: nss
-        } -> without_command
-               (Middle { prev = cs :: pss;
-                         curr = ns, (Apply e);
-                         next = nss
-                       })
-      | Middle 
-        { prev = pss;
-          curr = cs, (Move e);
-          next = []
-        } -> without_command
-               (RightEnd { prev = cs, pss;
-                           curr = Apply e;
-                         })
+      | Middle  { prev = pss;
+                  curr = cs, (Move e);
+                  next = ns :: nss
+                } -> middle { prev = cs :: pss;
+                              curr = ns, (Apply e);
+                              next = nss
+                            }
+      | Middle  { prev = pss;
+                  curr = cs, (Move e);
+                  next = []
+                } ->  right { prev = cs, pss;
+                              curr = Apply e;
+                            }
 
-      | LeftEnd
-        { curr = Move e;
-          next = ns, nss
-        } -> without_command
-               (Middle { prev = [];
-                         curr = ns, (Apply e);
-                         next = nss
-                       })
-      | RightEnd
-        { prev = ps, pss;
-          curr = Move e
-        } -> without_command
-               (Middle { prev = pss;
-                         curr = ps, (Apply e);
-                         next = []
-                       })
+      | Left    { curr = Move e;
+                  next = ns, nss
+                } -> middle { prev = [];
+                              curr = ns, (Apply e);
+                              next = nss
+                            }
+      | Right   { prev = ps, pss;
+                  curr = Move e
+                } -> middle { prev = pss;
+                              curr = ps, (Apply e);
+                              next = []
+                            }
 
-      | Middle
-        ({ curr = (Perform cmd), (Apply ((Call _) as e))
-         } as o) -> with_command cmd
-                      (Middle
-                         { o with
-                           curr = (Perform cmd), (Move e)
-                         })
-      | Middle
-        ({ curr = (Execute (proc, ews)), (Apply (Call e)) 
-         } as o) -> let ew, ef = Energy.find e in
-                    without_command
-                      (Middle
-                         { o with
-                           curr = (Execute (proc, (ew :: ews))), 
-                                  (Move (Find (ef, Forward, proc)))
-                         })
-      | Middle
-        ({ curr = ((Declare _) as cs), (Apply (Call e)) 
-         } as o) -> let ec = Energy.close e in
-                    without_command
-                      (Middle
-                         { o with
-                           curr = cs, (Move (Close ec))
-                         })
-      | RightEnd
-        ({ curr = Apply (Call e) 
-         } as o) -> let ec = Energy.close e in
-                    without_command
-                      (RightEnd
-                         { o with
-                           curr = Move (Close ec)
-                         })
+      | Middle ({ curr = (Perform cmd), (Apply ((Call _) as e)) } as o)
+                  -> Ok { command = Some cmd;
+                             tape = Middle { o with
+                                             curr = (Perform cmd), (Move e)
+                        }}
+      | Middle ({ curr = (Execute (proc, ews)), (Apply (Call e)) } as o)
+                  -> let ew, ef = Energy.find e in
+                     middle { o with
+                              curr = (Execute (proc, (ew :: ews))), 
+                                     (Move (Find (ef, Forward, proc)))
+                            }
+      | Middle ({ curr = ((Declare _) as cs), (Apply (Call e)) } as o)
+                  -> let ec = Energy.close e in
+                     middle { o with
+                              curr = cs, (Move (Close ec))
+                            }
+      | Right  ({ curr = Apply (Call e) } as o)
+                  -> let ec = Energy.close e in
+                     right  { o with
+                              curr = Move (Close ec)
+                            }
 
-      | Middle
-        ({ curr = (Declare (proc, eos)), 
-                  (Apply (Find (e, _, proc'))) 
-         } as o) when proc = proc'
-                 -> let eo, ec = Energy.open' e in
-                    without_command
-                      (Middle
-                         { o with
-                           curr = (Declare (proc, (eo :: eos))), 
-                                  (Move (Call ec))
-                         })
-      | LeftEnd
-        ({ curr = Apply (Find (_, _, proc))
-        }) -> Error (Procedure_not_found proc)
+      | Middle ({ curr = (Declare (proc, eos)), 
+                         (Apply (Find (e, _, proc'))) 
+                } as o) when proc = proc'
+                  -> let eo, ec = Energy.open' e in
+                     middle { o with
+                              curr = (Declare (proc, (eo :: eos))), 
+                                     (Move (Call ec))
+                            }
+      | Left    { curr = Apply (Find (_, _, proc))
+                } -> Error (Procedure_not_found proc)
 
-      | RightEnd
-        ({ curr = Apply (Find (e, _, proc))
-         } as o) -> without_command
-                      (RightEnd
-                         { o with
-                           curr = Move (Find (e, Back, proc))
-                         })
+      | Right  ({ curr = Apply (Find (e, _, proc)) } as o)
+                  ->  right { o with
+                              curr = Move (Find (e, Back, proc))
+                            }
 
-      | Middle
-        ({ curr = ((Declare (proc, (oe :: oes))) as cs), 
-                  (Apply (Close e)) 
-         } as o) -> without_command
-                      ( match Energy.return oe e with
-                        | None    ->
-                           Middle
-                             { o with
-                               curr = cs, (Move (Close e))
-                             }
-                        | Some er ->
-                           Middle
-                             { o with
-                               curr = (Declare (proc, oes)), 
-                                      (Move (Return (er, Back)))
-                             })
-      | Middle
-        ({ curr = ((Execute (proc, (ew :: ews))) as cs), 
+      | Middle ({ curr = ((Declare (proc, (oe :: oes))) as cs), 
+                         (Apply (Close e)) 
+                } as o)
+                  -> middle { o with
+                              curr = match Energy.return oe e with
+                                     | None    -> cs, (Move (Close e))
+                                     | Some er -> (Declare (proc, oes)), 
+                                                  (Move (Return (er, Back)))
+                            }
+      | Middle ({ curr = ((Execute (proc, (ew :: ews))) as cs), 
                   (Apply ((Return (e, _)) as er)) 
-         } as o) -> without_command
-                      ( match Energy.done' ew e with
-                        | None    ->
-                           Middle
-                             { o with
-                               curr = cs, (Move er)
-                             }
-                        | Some ec ->
-                           Middle
-                             { o with
-                               curr = (Execute (proc, ews)), 
-                                      (Move (Call ec))
-                             })
-      | LeftEnd
-        ({ curr = Apply (Return (e, _)) 
-         } as o) -> without_command
-                      (LeftEnd
-                         { o with
-                           curr = Move (Return (e, Forward)) 
-                         })
+                } as o)
+                  -> middle { o with
+                              curr = match Energy.done' ew e with
+                                     | None    -> cs, (Move er)
+                                     | Some ec -> (Execute (proc, ews)),
+                                                  (Move (Call ec))
+                            }
+      | Left   ({ curr = Apply (Return (e, _)) } as o)
+                  ->   left { o with
+                              curr = Move (Return (e, Forward)) 
+                            }
 
-      | Middle
-        ({ curr = cs, (Apply e) 
-         } as o) -> without_command 
-                      (Middle
-                         { o with
-                           curr = cs, (Move e)
-                         })
-      | LeftEnd
-        ({ curr = Apply e 
-         } as o) -> without_command 
-                      (LeftEnd
-                         { o with
-                           curr = Move e
-                         })
-      | RightEnd
-        ({ curr = Apply e 
-         } as o) -> without_command 
-                      (RightEnd
-                         { o with
-                           curr = Move e
-                         })
+      | Middle ({ curr = cs, (Apply e) } as o)
+                  -> middle { o with
+                              curr = cs, (Move e)
+                            }
+      | Left   ({ curr = Apply e } as o)
+                  ->   left { o with
+                              curr = Move e
+                            }
+      | Right  ({ curr = Apply e } as o)
+                  ->  right { o with
+                              curr = Move e
+                            }
 
     let start tape =
       succ { tape; command = None }
+      
     end
 
   module Step = struct
@@ -276,6 +227,5 @@ module Make = functor (Num : SEQUENTIAL.T) -> struct
     let adjust tick = up_to_command 1 (Ok tick)
     let succ      o = up_to_command 1 (Tick.start o.tape)
     let tick      o = Tick.start o.tape
-    end
-            
+    end         
   end

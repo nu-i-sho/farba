cfa63dc3b03e65ac0650cb5c60c4b65d8a41d7df
Valentyn Zakharenko <vvzakharenko@gmail.com>
2023-11-12 19:04:15 +0200

    reimplement HexMap.Coord as 2D point

--- src/core/hexMap.ml
+++ src/core/hexMap.ml
@@ -1,37 +1,21 @@
 module Coord = struct
   type t = int * int
 
-  let zero = 0, 0
+  let zero = 1, 1
 
-  let compare (i1, j1) (i2, j2) =
-    match  compare i1 i2 with
-    | 0 -> compare j1 j2
-    | x -> x
+  let compare (x1, y1) (x2, y2) =
+    match  compare x1 x2 with
+    | 0 -> compare y1 y2
+    | n -> n
           
-  let move side (i, j) =
-    let di, dj =
-      match (j mod 2 = 0), side with
-        
-      |     _, Side.Up        ->  0, -1
-      |     _, Side.Down      ->  0, +1
-  
-      |  true, Side.LeftUp    -> -1, -1
-      |  true, Side.RightUp   -> +1, -1
-      |  true, Side.LeftDown  -> -1,  0
-      |  true, Side.RightDown -> +1,  0
-                           
-      | false, Side.LeftUp    -> -1,  0
-      | false, Side.RightUp   -> +1,  0
-      | false, Side.LeftDown  -> -1, +1
-      | false, Side.RightDown -> +1, +1 in
-    
-    i + di,
-    j + dj
-
-  let to_2D eR iR (i, j) = 
-    iR * (i * 2 + 1),
-    eR * ((j + (i mod 2)) * 2 + abs ((i mod 2) - 1))
-    
+  let move side (x, y) =
+    match side with    
+    | Side.Up        -> x    , y - 2
+    | Side.Down      -> x    , y + 2
+    | Side.RightDown -> x + 2, y + 1
+    | Side.LeftUp    -> x - 2, y - 1
+    | Side.RightUp   -> x + 2, y - 1
+    | Side.LeftDown  -> x - 2, y + 1
   end
 
 include Map.Make(Coord)
